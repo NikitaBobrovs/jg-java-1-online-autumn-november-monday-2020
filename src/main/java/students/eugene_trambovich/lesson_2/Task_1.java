cc7144c7ffde8d816a0fc2c2ba7441040f4d7d26
Eugene Trambovich <evgeny.t@20bet.com>
2020-11-10 11:39:33 +0200

    level-1

---
+++ src/main/java/students/eugene_trambovich/lesson_2/Task_1.java
@@ -0,0 +1,13 @@
+package students.eugene_trambovich.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+        int yourAge = scanner.nextInt();
+        System.out.println("Your age is: " + yourAge);
+    }
+}
