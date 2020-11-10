b879e9f4aaa9e5d667d3f5fbb8909559c303b0c7
janase1 <janayanas21@gmail.com>
2020-11-10 16:17:35 +0200

    lesson 2 level 6 task 23

---
+++ src/main/java/students_jana_sergejenko_lesson_2_level_6_task_23/AttentionPlease.java
@@ -0,0 +1,7 @@
+package students_jana_sergejenko_lesson_2_level_6_task_23;
+
+class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
