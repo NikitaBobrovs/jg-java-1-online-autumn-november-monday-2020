a5dee484dfe0b9ca41a64dfc41de2859af12f7d8
Jkaverska <jelena.kaverska@28stone.com>
2020-11-10 14:10:06 +0200

    Lesson 2 level 1 - task 2 and all level 2 tasks

---
+++ src/main/java/students/jelena_kaverska/lesson_2/level_2/task_4/Phone.java
@@ -0,0 +1,4 @@
+package students.jelena_kaverska.lesson_2.level_2.task_4;
+
+class Phone{
+}
---
+++ src/main/java/students/jelena_kaverska/lesson_2/level_2/task_8/Task8.java
@@ -0,0 +1,11 @@
+package students.jelena_kaverska.lesson_2.level_2.task_8;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        System.out.print("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello " + scanner.next() + "!");
+    }
+}
