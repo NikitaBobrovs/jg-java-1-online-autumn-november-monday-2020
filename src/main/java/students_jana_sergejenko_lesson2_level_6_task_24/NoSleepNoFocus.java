984dae714735b42703d40db8351d6236904bdf7b
janase1 <janayanas21@gmail.com>
2020-11-10 16:19:03 +0200

    lesson 2 level 6 task 24

---
+++ src/main/java/students_jana_sergejenko_lesson2_level_6_task_24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package students_jana_sergejenko_lesson2_level_6_task_24;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
