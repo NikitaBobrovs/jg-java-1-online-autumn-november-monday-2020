949bf9ba8ed34b7820e2f5edda957e81055b69a5
janase1 <janayanas21@gmail.com>
2020-11-10 20:02:51 +0200

    lesson 1 task 11,12

---
+++ src/main/java/jana_sergejenko/lesson_1/task_11/Sum.java
@@ -0,0 +1,14 @@
+package jana_sergejenko.lesson_1.task_11;
+
+public class Sum {
+    public static void main(String[] args) {
+        int num1=167;
+        int num2=54;
+
+        int result1=num1+num2;
+        int result2=num1/num2;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+}
