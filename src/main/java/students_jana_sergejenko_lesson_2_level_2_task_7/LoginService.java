8e8182745f9ad4d7fa1d3203207d9275d8a4b7e5
janase1 <janayanas21@gmail.com>
2020-11-10 14:02:16 +0200

    lesson 2 level 2 task 6

---
+++ src/main/java/students_jana_sergejenko_lesson_2_level_2_task_7/LoginService.java
@@ -0,0 +1,10 @@
+package students_jana_sergejenko_lesson_2_level_2_task_7;
+
+class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
+
+
+
