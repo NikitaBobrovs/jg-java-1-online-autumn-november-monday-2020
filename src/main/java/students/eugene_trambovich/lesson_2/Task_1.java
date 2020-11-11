b224f1cca0406e1aa80ca83cf7767025d0061f2d
Eugene Trambovich <evgeny.t@20bet.com>
2020-11-11 13:21:05 +0200

    Task_1_fix

--- src/main/java/students/eugene_trambovich/lesson_2/Task_1.java
+++ src/main/java/students/eugene_trambovich/lesson_2/Task_1.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 public class Task_1 {
     public static void main(String[] args) {
 
-        System.out.println("Enter your age: ");
         Scanner scanner = new Scanner(System.in);
-        int yourAge = scanner.nextInt();
-        System.out.println("Your age is: " + yourAge);
+
+        System.out.println("Enter 1st integer: ");
+        int firstInteger = scanner.nextInt();
+
+        System.out.println("Enter 2nd integer: ");
+        int secondInteger = scanner.nextInt();
+
+        int addition = firstInteger + secondInteger;
+        int subtraction = firstInteger - secondInteger;
+        int multiplication = firstInteger * secondInteger;
+        int division = firstInteger / secondInteger;
+
+        System.out.println("Addition result is: " + addition);
+        System.out.println("Subtraction result is: " + subtraction);
+        System.out.println("Multiplication result is: " + multiplication);
+        System.out.println("Division result is: " + division);
     }
 }
