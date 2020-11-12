8ae2673c2bc28bca7af3899703fac280fc1b136c
Jkaverska <lena.kaverski@gmail.com>
2020-11-12 16:10:17 +0200

    Lesson 2 levels 5-7

---
+++ src/main/java/students/jelena_kaverska/lesson_2/level_6/task_24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package students.jelena_kaverska.lesson_2.level_6.task_24;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
