bc502f14321d80117edd4c0160f4eefad5484753
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-12 09:57:57 +0200

    Lesson 2 level 2 task 8

---
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
@@ -0,0 +1,12 @@
+package students.polina_ivashkevich.lesson_2.level_2.task_8;
+
+import java.sql.SQLOutput;
+
+public class Name {
+    public static void main(String[] args) {
+        String s = new String();
+        s="Polina";
+        System.out.println("Hello" + "_" + s);
+    }
+
+}
