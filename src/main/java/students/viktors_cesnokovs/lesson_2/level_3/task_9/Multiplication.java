ed80afc4879e88f6f98506b250b495874f895f74
ViktorChesnokov <doc18@inbox.lv>
2020-11-14 00:21:49 +0200

    lesson 2 level 3 task 9

---
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_3/task_9/Multiplication.java
@@ -0,0 +1,12 @@
+package students.viktors_cesnokovs.lesson_2.level_3.task_9;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int num = 8;
+        for (int i = 1; i <= 10; ++i) {
+            System.out.println(num + "*" + i + "= " + (num * i));
+
+        }
+    }
+}
+
