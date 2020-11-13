623617a31541cb34e639d7a762910d57872f891e
ViktorChesnokov <doc18@inbox.lv>
2020-11-13 23:22:00 +0200

    lesson 2 level 2 task 7

---
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_2/task_7/LoginService.java
@@ -0,0 +1,10 @@
+package students.viktors_cesnokovs.lesson_2.level_2.task_7;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
