d34cd434b1c995e9974809a6bf867a02ef7492d8
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-13 12:35:49 +0200

    Lesson 2 level 2 task 8

--- src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
@@ -1,7 +1,5 @@
 package students.polina_ivashkevich.lesson_2.level_2.task_8;
 
-import java.sql.SQLOutput;
-
 public class Name {
     public static void main(String[] args) {
         String s = new String();
