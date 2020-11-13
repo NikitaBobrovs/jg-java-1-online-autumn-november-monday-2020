2bfcaccb4b319ddf86eca4127eca9805fdaece0e
NikitaBobrovs <nekit1234@gmail.com>
2020-11-13 20:03:55 +0200

    Lesson 2 level 2 task 7

---
+++ src/main/java/students/nikita_bobrovs/lesson_2/level_2/task_7/LoginService.java
@@ -0,0 +1,8 @@
+package students.nikita_bobrovs.lesson_2.level_2.task_7;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
