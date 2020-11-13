abd306c3b71cabf06852f349bf7140aa91da424f
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-13 12:22:26 +0200

    Lesson 2 level 5 task 18

--- src/main/java/students/polina_ivashkevich/lesson_2/level_5/task_15/Text.java
+++
@@ -1,7 +0,0 @@
-package students.polina_ivashkevich.lesson_2.level_5.task_15;
-
-public class Text {
-    public static void main(String[] args) {
-        System.out.print("text");
-    }
-}
---
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_5/task_18/SizeMatters.java
@@ -0,0 +1,10 @@
+package students.polina_ivashkevich.lesson_2.level_5.task_18;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
+
+
