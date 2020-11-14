7c86d4cdc523e9ddf3aa97a2898277bdbc80c138
Cath1177 <alexkatya77@gmail.com>
2020-11-14 12:04:23 +0200

    lesson2 level6 task23

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_2/level_6/task23/AttentionPlease.java
@@ -0,0 +1,7 @@
+package students.jekaterina_aleksejeva.lesson_2.level_6.task23;
+
+public class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
