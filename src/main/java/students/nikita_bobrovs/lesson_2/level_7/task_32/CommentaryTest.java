26bcb7418b4f5a8f6de7ef44db83ed95ef3f866b
NikitaBobrovs <nekit1234@gmail.com>
2020-11-14 22:06:27 +0200

    Lesson 2 level 7 task 32

---
+++ src/main/java/students/nikita_bobrovs/lesson_2/level_7/task_32/CommentaryTest.java
@@ -0,0 +1,16 @@
+package students.nikita_bobrovs.lesson_2.level_7.task_32;
+
+public class CommentaryTest {
+    public static void main(String[] args) {
+        /* Testing printout of two set variables.
+           Firstly set up two integer variables.
+           Then do 'System.out.println' for each variable.
+           Finally run test.
+         */
+        int firstNumber = 5;     //Make sure to choose int as variable type
+        int secondNumber = 10;
+        System.out.println("First number is : "+firstNumber);
+        System.out.println("Second number is : "+secondNumber);
+        //After successful test, save project.
+    }
+}
