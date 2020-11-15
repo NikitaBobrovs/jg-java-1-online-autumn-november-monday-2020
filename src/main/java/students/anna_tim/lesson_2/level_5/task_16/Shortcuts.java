1873addb5422da3f12f70819a01c03eff7c79f82
Antimka <antimkin@gmail.com>
2020-11-15 22:12:16 +0200

    adding task11 of lesson2

---
+++ src/main/java/students/anna_tim/lesson_2/level_5/task_16/Shortcuts.java
@@ -0,0 +1,7 @@
+package students.anna_tim.lesson_2.level_5.task_16;
+
+public class Shortcuts {
+
+    //Alt (слева) + 1 - открывает окно Project в котором видна структура проекта.
+    //Alt+Insert - вызывает всплывающее меню и стрелочами можно выбрать “Java class” / <enter>.
+}
