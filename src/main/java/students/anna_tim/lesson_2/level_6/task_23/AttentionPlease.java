150192c2f854f9454f45968f50e3705354314f6a
Antimka <antimkin@gmail.com>
2020-11-15 23:04:00 +0200

    adding task23 of lesson2

---
+++ src/main/java/students/anna_tim/lesson_2/level_6/task_23/AttentionPlease.java
@@ -0,0 +1,9 @@
+package students.anna_tim.lesson_2.level_6.task_23;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not compile!");
+    }
+}
