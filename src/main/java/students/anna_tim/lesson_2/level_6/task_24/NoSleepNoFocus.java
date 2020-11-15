1d65a68c9cf746b0ab0402e50da6fd3c76caa318
Antimka <antimkin@gmail.com>
2020-11-15 23:07:32 +0200

    adding task24 of lesson2

--- src/main/java/students/anna_tim/lesson_2/level_6/task_24/NoSleepNoFocus.java
+++ src/main/java/students/anna_tim/lesson_2/level_6/task_24/NoSleepNoFocus.java
@@ -3,6 +3,6 @@ package students.anna_tim.lesson_2.level_6.task_24;
 public class NoSleepNoFocus {
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
-        System.out.println("So bad");
+        System.out.println("So bad..");
     }
 }
