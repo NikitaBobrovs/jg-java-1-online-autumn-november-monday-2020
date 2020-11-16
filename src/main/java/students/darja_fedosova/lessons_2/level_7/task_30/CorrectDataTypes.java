aedd68d5c93bcac849f92cf2f8caa9706a83c286
darjafedosova <fedosova.d@gmail.com>
2020-11-17 00:16:06 +0200

    Lessons_2 Level_7 Task_30

---
+++ src/main/java/students/darja_fedosova/lessons_2/level_7/task_30/CorrectDataTypes.java
@@ -0,0 +1,16 @@
+package students.darja_fedosova.lessons_2.level_7.task_30;
+
+public class CorrectDataTypes {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        long numberTwo = 20L;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        long sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}
