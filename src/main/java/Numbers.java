65e0237899c3d5a4edf9abf7ba298f17812efad1
topchilina <topchilina@gmail.com>
2020-11-16 15:57:17 +0200

    Lesson_2.level_1.task_1

---
+++ src/main/java/Numbers.java
@@ -0,0 +1,22 @@
+public class Numbers {
+    public static void main (String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("first number ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("second number ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = (firstNumber + secondNumber);
+        int subtraction = (firstNumber - secondNumber);
+        int multiplication = (firstNumber * secondNumber);
+        int division = (firstNumber / secondNumber);
+
+        System.out.print = ("Sum result :" + sum);
+        System.out.print = ("Subtraction result :" + subtraction);
+        System.out.print = ("Multiplication result :" + multiplication);
+        System.out.print = ("Division result :" + division);
+    }
+}
+
