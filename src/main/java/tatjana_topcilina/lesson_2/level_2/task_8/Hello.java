df6bc14b46dbba0163de5de8ec40cff2ff1488c6
topchilina <topchilina@gmail.com>
2020-11-16 16:47:52 +0200

    Lesson_2.level_2.task_8

---
+++ src/main/java/tatjana_topcilina/lesson_2/level_2/task_8/Hello.java
@@ -0,0 +1,11 @@
+package tatjana_topcilina.lesson_2.level_2.task_8;
+
+public class Hello {
+    public static void main (String[] arg) {
+        System.out.println("Enter Your username");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next.Line();
+        System.out.println("Hello"+userName+"!");
+
+    }
+}
