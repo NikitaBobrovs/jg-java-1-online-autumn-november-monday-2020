91239eea8f20ef09463904c9c98fc6a8db01dc5a
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-16 23:40:42 +0200

    Lesson 2 level 6 task 24

---
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_6/task_24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package students.polina_ivashkevich.lesson_2.level_6.task_24;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
