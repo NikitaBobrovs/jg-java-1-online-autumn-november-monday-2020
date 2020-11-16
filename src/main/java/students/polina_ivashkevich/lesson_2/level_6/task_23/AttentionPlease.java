b76c2531c79a07720f01d20beb19c31dc774eb6b
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-16 23:33:34 +0200

    Lesson 2 level 6 task 23

--- src/main/java/students/polina_ivashkevich/lesson_2/level_6/task_23/AttentionPlease.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_6/task_23/AttentionPlease.java
@@ -2,6 +2,7 @@ package students.polina_ivashkevich.lesson_2.level_6.task_23;
 
 public class AttentionPlease {
     public static void main(String[] args) {
-        System.out.println("Code outside the method will not compile!");
+    System.out.println("Code outside the method will not compile!");
     }
 }
+
