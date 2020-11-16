e87f34019a9eadb1d66e2b69f96463c1c39f30c3
darjafedosova <fedosova.d@gmail.com>
2020-11-17 00:02:30 +0200

    Lessons_2 Level_6 Task_24

---
+++ src/main/java/students/darja_fedosova/lessons_2/level_6/task_24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package students.darja_fedosova.lessons_2.level_6.task_24;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
