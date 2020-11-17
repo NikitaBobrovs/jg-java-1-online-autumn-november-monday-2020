a3b603f801946d7da8e1a9981e2e4682572c7a78
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-17 13:28:46 +0200

    Lesson 2 level 7 task 32

---
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_7/task_32/CommentType.java
@@ -0,0 +1,11 @@
+package students.polina_ivashkevich.lesson_2.level_7.task_32;
+
+public class CommentType {//название класса CommentType
+
+    public static void main(String[] args) {
+        /*Теперь,мне нужно вывести в консоль :
+        "This is example which comment type i can use!"
+         */
+        System.out.println("This is example which comment type i can use!");
+    }
+}
