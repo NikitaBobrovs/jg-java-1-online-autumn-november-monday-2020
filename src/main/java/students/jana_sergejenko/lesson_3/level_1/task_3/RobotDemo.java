8292130687238503630f0f4cd491ceb98f74da3e
janase1 <janayanas21@gmail.com>
2020-11-18 00:50:35 +0200

    lesson 3 level 1 task 1,2,3,4,5,6

---
+++ src/main/java/students/jana_sergejenko/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.jana_sergejenko.lesson_3.level_1.task_3;
+
+import students.jana_sergejenko.lesson_3.level_1.task_1.Robot;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot myRobot=new Robot();
+    }
+}
