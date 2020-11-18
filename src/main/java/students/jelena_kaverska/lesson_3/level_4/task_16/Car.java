59054a037fc5abcb8f88889d2f12429677d02d4c
Jkaverska <lena.kaverski@gmail.com>
2020-11-18 18:49:27 +0200

    Lesson 3 level 3 & 4: Tasks: 14-15 and class from level 1 update

---
+++ src/main/java/students/jelena_kaverska/lesson_3/level_4/task_16/Car.java
@@ -0,0 +1,21 @@
+package students.jelena_kaverska.lesson_3.level_4.task_16;
+
+class Car {
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+}
+
+class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+}
