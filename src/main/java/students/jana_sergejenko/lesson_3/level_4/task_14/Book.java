04d0d90c750ccfb0ac647d723b4878ee3fe59bed
janase1 <janayanas21@gmail.com>
2020-11-18 20:31:11 +0200

    lesson 3 level 4 task 14

---
+++ src/main/java/students/jana_sergejenko/lesson_3/level_4/task_14/Book.java
@@ -0,0 +1,15 @@
+package students.jana_sergejenko.lesson_3.level_4.task_14;
+
+class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
