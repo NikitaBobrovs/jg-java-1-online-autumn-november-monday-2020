d5cb3fa9784b6a61c06b0483a6e4f94910b0f99d
janase1 <janayanas21@gmail.com>
2020-11-18 20:36:14 +0200

    lesson 3 level 4 task 16

---
+++ src/main/java/students/jana_sergejenko/lesson_3/level_4/task_16/Car.java
@@ -0,0 +1,15 @@
+package students.jana_sergejenko.lesson_3.level_4.task_16;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
