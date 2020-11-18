a782bd817c3a54c880cb9e3fb81a4d038979cde0
janase1 <janayanas21@gmail.com>
2020-11-18 20:33:45 +0200

    lesson 3 level 4 task 15

---
+++ src/main/java/students/jana_sergejenko/lesson_3/level_4/task_15/PhoneDemo.java
@@ -0,0 +1,11 @@
+package students.jana_sergejenko.lesson_3.level_4.task_15;
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
