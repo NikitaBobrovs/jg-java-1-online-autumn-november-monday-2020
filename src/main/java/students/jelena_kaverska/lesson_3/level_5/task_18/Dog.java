7d48cdffc3926ea891ec8525a90bd9f60a50079c
Jkaverska <lena.kaverski@gmail.com>
2020-11-18 19:55:08 +0200

    Lesson 3 full level 5

---
+++ src/main/java/students/jelena_kaverska/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,15 @@
+package students.jelena_kaverska.lesson_3.level_5.task_18;
+
+public class Dog {
+    String name;
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public void voice(){
+        System.out.println(this.name);
+        System.out.println(this.name);
+        System.out.println(this.name);
+    }
+}
