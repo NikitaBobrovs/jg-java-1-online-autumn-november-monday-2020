245b4082bffff22f67e5ac5f84ba89e601636ae6
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-19 18:03:18 +0200

    Lesson 3 level 2 task 7

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_2/task_7/String1.java
@@ -0,0 +1,9 @@
+package students.polina_ivashkevich.lesson_3.level_2.task_7;
+
+public class String1 {
+    public static void main(String[] args) {
+        String myName="Polina";
+        System.out.println(myName);
+    }
+
+}
