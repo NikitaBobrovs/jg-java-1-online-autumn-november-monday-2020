b72b1ee3d29a4e131ebfbe78bf702dbd2f75e8c9
ViktorChesnokov <doc18@inbox.lv>
2020-11-19 19:45:06 +0200

    lesson 2 level 3 task 9

--- src/main/java/students/viktors_cesnokovs/lesson_2/level_3/task_9/Multiplication.java
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_3/task_9/Multiplication.java
@@ -1,10 +1,14 @@
 package students.viktors_cesnokovs.lesson_2.level_3.task_9;
 
+import java.util.Scanner;
+
 public class Multiplication {
     public static void main(String[] args) {
-        int num = 8;
+        System.out.println("Vvedite celoe chislo: ");
+        Scanner scan = new Scanner(System.in);
+        int cislo = scan.nextInt();
         for (int i = 1; i <= 10; ++i) {
-            System.out.println(num + "*" + i + "= " + (num * i));
+            System.out.println(cislo + "*" + i + "= " + (cislo * i));
 
         }
     }
