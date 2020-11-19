c8987c06bc439c10b3528724fe230cd12beb2784
Cath1177 <alexkatya77@gmail.com>
2020-11-19 16:31:52 +0200

    lesson3 leve1 3 Robots with different names

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,19 @@
+package students.jekaterina_aleksejeva.lesson_3.level_1;
+
+import students.jekaterina_aleksejeva.lesson_3.level_1.Robot;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robotDemo = new Robot();
+        robotDemo.sayHello();
+        robotDemo.sayYourName();
+
+        Robot robotDemo1 = new Robot();
+        robotDemo1.sayHello();
+        robotDemo1.sayYourName1();
+
+        Robot robotDemo2 = new Robot();
+        robotDemo2.sayHello();
+        robotDemo1.sayYourName2();
+    }
+}
