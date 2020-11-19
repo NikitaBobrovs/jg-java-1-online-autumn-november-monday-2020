bbad7a700627e7a636072acdafd47a2a16152227
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-19 17:31:45 +0200

    Lesson 3 level 1 task 3

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,9 @@
+package students.polina_ivashkevich.lesson_3.level_1.task_3;
+
+import students.polina_ivashkevich.lesson_3.level_1.task_2.Robot;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot myRobot = new Robot();
+    }
+}
