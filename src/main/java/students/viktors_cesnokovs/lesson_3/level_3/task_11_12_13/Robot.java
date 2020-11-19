19fcad43841dfde3a681732fb94b65db761888cc
ViktorChesnokov <doc18@inbox.lv>
2020-11-19 21:58:05 +0200

    lesson 3 level 3 task 11,12,13

---
+++ src/main/java/students/viktors_cesnokovs/lesson_3/level_3/task_11_12_13/Robot.java
@@ -0,0 +1,14 @@
+package students.viktors_cesnokovs.lesson_3.level_3.task_11_12_13;
+
+public class Robot {
+    String name;                                        //Task 11
+    Robot(String robotName){this.name=robotName;        //Task 12
+    }
+    public void sayHello(){
+        System.out.println("Hello!"); }
+        public Robot(){
+    }
+    public void sayYourName(){
+        System.out.println("My name is " + this.name);  //Task 13
+    }
+}
