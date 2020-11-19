749e3ec4f3fe9278ab5cbde91f7402fe96b12713
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-19 17:56:14 +0200

    Lesson 3 level 1 task 6

--- src/main/java/students/polina_ivashkevich/lesson_3/level_1/task_2/Robot.java
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_1/task_2/Robot.java
@@ -2,7 +2,10 @@ package students.polina_ivashkevich.lesson_3.level_1.task_2;
 
 public class Robot {
     String name;
-   public void sayYourName() {
-       System.out.println("\"My name is ROBO\"");
-   }
-}
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+        public void sayYourName() {
+            System.out.println("\"My name is ROBO\"");
+        }
+    }
--- src/main/java/students/polina_ivashkevich/lesson_3/level_1/task_3/RobotDemo.java
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_1/task_3/RobotDemo.java
@@ -4,7 +4,12 @@ import students.polina_ivashkevich.lesson_3.level_1.task_2.Robot;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot myRobot = new Robot();
-        myRobot.sayYourName();
+        Robot Robot1 = new Robot();
+        Robot1.sayHello();
+        Robot1.sayYourName();
+
+        Robot Robot2 = new Robot();
+        Robot2.sayHello();
+        Robot2.sayYourName();
     }
 }
