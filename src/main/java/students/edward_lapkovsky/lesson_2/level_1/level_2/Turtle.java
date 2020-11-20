398454521c074981131247bd1c6f9cb131e8362d
Edward <lpvsky@gmail.com>
2020-11-20 17:24:18 +0200

    lesson 2, level 2

--- src/main/java/students/edward_lapkovsky/lesson_2/level_1/level_2/Turtle.java
+++ src/main/java/students/edward_lapkovsky/lesson_2/level_1/level_2/Turtle.java
@@ -2,8 +2,6 @@ package students.edward_lapkovsky.lesson_2.level_1.level_2;
 
 class Turtle {
 
-    public static void main(String[] args){
-
-}
-
+    public static void main(String[] args) {
+    }
 }
