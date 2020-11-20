d5213cf8470b9eeabe5e8c203c5a1150b408624e
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-20 21:08:16 +0200

    Lesson 3 level 3 task 13

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_3/task_13/Robot3Demo.java
@@ -0,0 +1,8 @@
+package students.polina_ivashkevich.lesson_3.level_3.task_13;
+
+public class Robot3Demo {
+    public static void main(String[] args) {
+       Robot3 robot = new Robot3("Anna!");
+       robot.sayYourName();
+    }
+}
