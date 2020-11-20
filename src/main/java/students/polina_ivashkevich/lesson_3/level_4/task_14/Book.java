5f9ab1b9fff926d8a3009509b9df64c8fa5b4fa5
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-20 21:12:00 +0200

    Lesson 3 level 3 task 14

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_4/task_14/Book.java
@@ -0,0 +1,25 @@
+package students.polina_ivashkevich.lesson_3.level_4.task_14;
+
+public class Book {
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
+
