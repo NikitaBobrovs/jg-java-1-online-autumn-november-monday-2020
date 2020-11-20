5a436c0d7532f761ed1997b05e77d0cbb413d0a5
Edward <lpvsky@gmail.com>
2020-11-20 16:49:12 +0200

    lesson 2, level 1, task1

---
+++ src/main/java/students/edward_lapkovsky/lesson_2/level_1/task_2/doubleNum.java
@@ -0,0 +1,31 @@
+
+
+package students.edward_lapkovsky.lesson_2.level_1.task_2;
+
+import java.util.Scanner;
+
+public class doubleNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter F number");
+        double a = scanner.nextDouble();
+        System.out.println("Enter S number");
+        double b = scanner.nextDouble();
+        double plus = a+b;
+        System.out.println("plus" +plus);
+        double minus = a-b;
+        System.out.println("minus" +minus);
+        double umn = a*b;
+        System.out.println("умножение" +umn);
+        double del = a/b;
+        System.out.println("деление" +del);
+        
+
+
+
+
+
+
+
+    }
+}
