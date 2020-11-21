4bd54f6f89ccde4c843564d01550845319940cd2
NikitaBobrovs <nekit1234@gmail.com>
2020-11-21 21:37:33 +0200

    Lesson 3 level 7 task 32 fixed

--- src/main/java/students/nikita_bobrovs/lesson_3/level_7/task_32/BankAccount.java
+++ src/main/java/students/nikita_bobrovs/lesson_3/level_7/task_32/BankAccount.java
@@ -5,19 +5,16 @@ class BankAccount {
     String owner;
     int money;
 
-    BankAccount(String owner,int moneyAmount) {
+    BankAccount(String owner,int money) {
         this.owner = owner;
-        this.money = moneyAmount;
+        this.money = money;
     }
-
     String getOwner() {
         return this.owner;
     }
-
-    int getMoney() {
-        return this.money;
+    String getMoney() {
+        return String.valueOf(this.money);
     }
-
 }
 
 class BankAccountDemo {
@@ -25,7 +22,7 @@ class BankAccountDemo {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount("Viktor", 100000);
         String owner = bankAccount.getOwner();
-        int money = bankAccount.getMoney();
+        String money = bankAccount.getMoney();
         System.out.println("Owner = " + owner);
         System.out.println("Money = " + money);
     }
