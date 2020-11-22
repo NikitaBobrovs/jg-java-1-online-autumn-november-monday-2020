83a2f27b302cef3d0db0ddee1ce5664f136aab30
Cath1177 <alexkatya77@gmail.com>
2020-11-22 17:40:12 +0200

    lesson3 level7 task30

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_3/level_7/task_30/Circle.java
@@ -0,0 +1,12 @@
+package students.jekaterina_aleksejeva.lesson_3.level_7.task_30;
+
+class Circle {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+    double calculateArea() {
+        return Math.PI*(radius*radius);
+    }
+}
