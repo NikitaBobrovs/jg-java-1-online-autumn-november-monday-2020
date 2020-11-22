95aa14847bb36eda56337c5a97ca4b249c45e6ef
Cath1177 <alexkatya77@gmail.com>
2020-11-22 16:31:08 +0200

    lesson3 level5 task20 fixed

--- src/main/java/students/jekaterina_aleksejeva/lesson_3/level_5/task_20/Dog.java
+++ src/main/java/students/jekaterina_aleksejeva/lesson_3/level_5/task_20/Dog.java
@@ -1,23 +1,24 @@
 package students.jekaterina_aleksejeva.lesson_3.level_5.task_20;
 
 class Dog {
-    String dogName;
-    Integer dogAge;
-    Integer newDogAge;
+    String name;
+    Integer age;
 
-    Dog(String dogName, Integer dogAge)
+    Dog(String name, Integer age)
     {
-        this.dogName = dogName;
-        this.dogAge = dogAge;
-        this.newDogAge = dogAge + 1;
+        this.name = name;
+        this.age = age;
     }
+
     public void getDogVoice () {
-        System.out.println("My name is " + dogName + "!" + "I am " + dogAge + " years old!");
+
+        System.out.println("My name is " + name + "!" + "I am " + age + " years old!");
     }
     void happyBirthday() {
-        System.out.println("Happy Birthday, your are " + newDogAge + " years old today!");
-        System.out.println("My name is " + dogName + "!" + "I am " + newDogAge + " years old!");
+        this.age = age + 1;
 
+        System.out.println("Happy Birthday, your are " + age + " years old today!");
+        System.out.println("My name is " + name + "!" + "I am " + age + " years old today!");
     }
 }
 
