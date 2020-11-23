cf9cbf6a1e25e80514fe6bb801057038f4918b84
Cath1177 <alexkatya77@gmail.com>
2020-11-23 11:15:39 +0200

    lesson3 level7 task32

---
+++ src/main/java/students/jekaterina_aleksejeva/level_7/task_32/BankAccount.java
@@ -0,0 +1,33 @@
+package students.jekaterina_aleksejeva.level_7.task_32;
+
+class BankAccount {
+
+    String owner;
+    int money;
+
+    BankAccount(String owner, int money) {
+        this.owner = owner;
+        this.money = money;
+    }
+
+    String getOwner() {
+        return this.owner;
+    }
+
+    int getMoney() {
+        return money;
+    }
+
+}
+
+class BankAccountDemo {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Viktor", 100000);
+        String owner = bankAccount.getOwner();
+        int money = bankAccount.getMoney();
+        System.out.println("Owner = " + owner);
+        System.out.println("Money = " + money);
+    }
+
+}
