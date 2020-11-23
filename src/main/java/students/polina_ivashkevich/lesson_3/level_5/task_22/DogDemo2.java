3234ee7c773443cfe844b09db9233207578d02e0
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-23 16:47:01 +0200

    Lesson 3 level 5 task 22

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_5/task_22/DogDemo2.java
@@ -0,0 +1,12 @@
+package students.polina_ivashkevich.lesson_3.level_5.task_22;
+
+public class DogDemo2 {
+    public static void main(String[] args) {
+        Dog2 c = new Dog2();
+        c.voice();
+        c.changeColor("Grey");
+        c.voice();
+
+
+    }
+}
