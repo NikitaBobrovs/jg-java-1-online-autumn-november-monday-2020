fa4a70c9c13b8acfe820de0a34ba2fa2eceec5be
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-23 12:40:53 +0200

    Lesson 3 level 5 task 20

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_5/task_20/Dog.java
@@ -0,0 +1,14 @@
+package students.polina_ivashkevich.lesson_3.level_5.task_20;
+
+public class Dog {
+    int age = 10;
+    public void voice () {
+        System.out.println("Sam!");
+        System.out.println(age);
+
+    }public void happyBirthday() {
+        System.out.println(age+1 +" Happy birthday!");
+    }public void voice1() {
+        System.out.println("Sam!");
+    }
+}
