ca4c8b4f329fcb15cd37cde6db98ea9bd131721d
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-23 12:23:56 +0200

    Lesson 3 level 5 task 18

---
+++ src/main/java/students/polina_ivashkevich/lesson_3_level_5/task_18/Dog.java
@@ -0,0 +1,9 @@
+package students.polina_ivashkevich.lesson_3_level_5.task_18;
+
+public class Dog {
+   public void voice() {
+       System.out.println("Sam");
+       System.out.println("Sam");
+       System.out.println("Sam");
+   }
+}
