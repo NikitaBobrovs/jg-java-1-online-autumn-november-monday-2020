bbf4dc96a193c1bb304205c6a28641a4c2ee03fc
ViktorChesnokov <doc18@inbox.lv>
2020-11-25 20:40:23 +0200

    lesson 2 level 7 task 30

---
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_7/task_30/CorrectDataTypes.java
@@ -0,0 +1,18 @@
+package students.viktors_cesnokovs.lesson_2.level_7.task_30;
+
+public class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
