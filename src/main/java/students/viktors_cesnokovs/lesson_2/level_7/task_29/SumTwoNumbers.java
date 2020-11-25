a69ddc3b0d44dfcdecd32ffd770493ac371168e8
ViktorChesnokov <doc18@inbox.lv>
2020-11-25 20:27:25 +0200

    lesson 2 level 7 task 29

---
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_7/task_29/SumTwoNumbers.java
@@ -0,0 +1,14 @@
+package students.viktors_cesnokovs.lesson_2.level_7.task_29;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        int sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+    }
+
+}
