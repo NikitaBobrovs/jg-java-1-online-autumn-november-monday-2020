f80c3c608f622e6bca8a051038a73b8dbf8445b6
ViktorChesnokov <doc18@inbox.lv>
2020-11-25 20:06:24 +0200

    lesson 2 level 5 task 18

---
+++ src/main/java/students/viktors_cesnokovs/lesson_2/level_5/task_18/SizeMatters.java
@@ -0,0 +1,8 @@
+package students.viktors_cesnokovs.lesson_2.level_5.task_18;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
