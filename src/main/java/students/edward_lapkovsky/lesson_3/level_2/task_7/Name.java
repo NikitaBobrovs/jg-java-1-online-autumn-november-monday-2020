43ec397db24056bef9484a3342364e422e80de47
Edward <lpvsky@gmail.com>
2020-11-27 20:12:39 +0200

    lesson_3_level2_task7,8,9

---
+++ src/main/java/students/edward_lapkovsky/lesson_3/level_2/task_7/Name.java
@@ -0,0 +1,9 @@
+package students.edward_lapkovsky.lesson_3.level_2.task_7;
+
+public class Name {
+    public static void main(String[] args) {
+        String myName = "Edward";
+        System.out.println(myName);
+
+    }
+}
---
+++ src/main/java/students/edward_lapkovsky/lesson_3/level_2/task_8/NewMyName.java
@@ -0,0 +1,10 @@
+package students.edward_lapkovsky.lesson_3.level_2.task_8;
+
+public class NewMyName {
+    public static void main(String[] args) {
+        String myName = "Edward";
+        String Hello = "Hi " + myName + "!";
+        System.out.println(Hello);
+
+    }
+}
