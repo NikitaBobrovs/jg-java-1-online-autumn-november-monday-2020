11fdeae48eea69457e8adfe6f6084754422ac5ab
Edward <lpvsky@gmail.com>
2020-11-27 19:56:57 +0200

    lesson_3

---
+++ src/main/java/students/edward_lapkovsky/lesson_3/level_1/task_1/Robot.java
@@ -0,0 +1,16 @@
+package students.edward_lapkovsky.lesson_3.level_1.task_1;
+
+public class Robot {
+    //task1
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    //task2
+    public Robot() {
+    }
+    //task5
+    public void sayYourName() {
+        System.out.println("MyNameIs ROBO");
+
+    }
+}
---
+++ src/main/java/students/edward_lapkovsky/lesson_3/level_1/task_1/RobotDemo.java
@@ -0,0 +1,18 @@
+package students.edward_lapkovsky.lesson_3.level_1.task_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        System.out.println("Robot");
+        Robot NewRobot = new Robot(); //task3
+        NewRobot.sayHello(); //task4
+        NewRobot.sayYourName(); //task5
+
+        Robot RobotBobot = new Robot(); //task6
+        RobotBobot.sayHello();
+        RobotBobot.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
