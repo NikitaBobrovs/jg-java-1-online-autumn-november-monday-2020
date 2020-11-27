04adba2372c9e2b45b2dca79a5c9914c86bc8db9
janase1 <janayanas21@gmail.com>
2020-11-27 13:26:27 +0200

    lesson 4 level 3 task 10

---
+++ src/main/java/students/jana_sergejenko/lesson_4/level_3/task_10/Program10.java
@@ -0,0 +1,24 @@
+package students.jana_sergejenko.lesson_4.level_3.task_10;
+
+import java.util.Scanner;
+
+public class Program10{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num = scan.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int num2 = scan.nextInt();
+        System.out.print("Enter 3rd number: ");
+        int num3 = scan.nextInt();
+        scan.close();
+
+        int min = num;
+        if (num2 < num)
+            min = num2;
+        else if (num3 <min)
+            min = num3;
+
+        System.out.println(min);
+    }
+}
