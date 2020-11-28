e125a670c982fcf5ced9400b6983f9aae4ef121b
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-11-28 18:33:43 +0200

    Lesson 3 level 5 task 18

---
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,9 @@
+package students.polina_ivashkevich.lesson_3.level_5.task_18;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog name = new Dog();
+        name.voice();
+
+    }
+}
