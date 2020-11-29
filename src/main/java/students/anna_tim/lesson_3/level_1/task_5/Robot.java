9ed8c9b4565140e82713602bd41ea427030d9346
Antimka <antimkin@gmail.com>
2020-11-29 21:48:37 +0200

    adding task5 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,16 @@
+package students.anna_tim.lesson_3.level_1.task_5;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/main/java/students/anna_tim/lesson_3/level_1/task_5/RobotDemo.java
@@ -0,0 +1,12 @@
+package students.anna_tim.lesson_3.level_1.task_5;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+
+        myRobot.sayHello();
+        myRobot.sayYourName();
+    }
+}
