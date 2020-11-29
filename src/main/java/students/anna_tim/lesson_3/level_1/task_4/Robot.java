ad46ebd126b3002073540aa130634ba6d5a7d3b6
Antimka <antimkin@gmail.com>
2020-11-30 01:48:32 +0200

    removing "public" from class definition

--- src/main/java/students/anna_tim/lesson_3/level_1/task_4/Robot.java
+++ src/main/java/students/anna_tim/lesson_3/level_1/task_4/Robot.java
@@ -1,12 +1,12 @@
 package students.anna_tim.lesson_3.level_1.task_4;
 
-public class Robot {
+class Robot {
 
-    public Robot() {
+    Robot() {
 
     }
 
-    public void sayHello() {
+    void sayHello() {
 
         System.out.println("Hello!");
 
