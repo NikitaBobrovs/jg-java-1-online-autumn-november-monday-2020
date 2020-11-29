d2c54bbc30ceba1cbe493293b0d4c3a438745c77
Antimka <antimkin@gmail.com>
2020-11-29 22:37:12 +0200

    adding task11 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_2/task_10/CompareStrings.java
@@ -0,0 +1,33 @@
+package students.anna_tim.lesson_3.level_2.task_10;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+     // equals() method
+        //boolean equals (Object str)
+        //It compares the content of the strings.
+     //  == operator
+        //The double equal (==) operator compares two object references to check whether they refer to same instance.
+     // CompareTo() method
+
+
+        // equals() method
+        String s = "Hell";
+        String s1 = "Hello";
+        String s2 = "Hello";
+        boolean b = s1.equals(s2);    //true
+        System.out.println(b);
+        b =	s.equals(s1) ;   //false
+        System.out.println(b);
+
+        //  == operator
+        String s3 = "Java";
+        String s4 = "Java";
+        String s5 = new String ("Java");
+        boolean c = (s3 == s4);     //true
+        System.out.println(c);
+        c =	(s3 == s5);      //false
+        System.out.println(c);
+
+    }
+}
+
---
+++ src/main/java/students/anna_tim/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,17 @@
+package students.anna_tim.lesson_3.level_3.task_11;
+
+public class Robot {
+
+    public Robot() {
+        // в этом свойстве наш виртуальный обьект робот будет запоминать своё имя.
+        String name;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
