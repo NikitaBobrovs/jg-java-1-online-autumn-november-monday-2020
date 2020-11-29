c2d905e366c422650e81bce05321cd18e9b25470
Antimka <antimkin@gmail.com>
2020-11-30 01:16:37 +0200

    adding task19 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_4/task_19/Dog.java
@@ -0,0 +1,23 @@
+package students.anna_tim.lesson_3.level_4.task_19;
+
+public class Dog {
+    String dogName;
+    int dogAge;
+
+    Dog(String dogName, int dogAge) {
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+    }
+
+    String getDogName() {
+        return this.dogName;
+    }
+
+    int getDogAge() {
+        return this.dogAge;
+    }
+
+    void voice () {
+        System.out.println("For voice command - name + age: " + this.dogName + " + " + this.dogAge + " years");
+    }
+}
