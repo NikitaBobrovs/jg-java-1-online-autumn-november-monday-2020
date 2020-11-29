b2f720e327f4582e82cee41f07890fac77b1c9d9
Antimka <antimkin@gmail.com>
2020-11-29 21:59:18 +0200

    adding task7 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_2/task_7/LearnString.java
@@ -0,0 +1,11 @@
+package students.anna_tim.lesson_3.level_2.task_7;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class LearnString {
+    public static void main(String[] args) {
+
+        String myName = "Anya";
+        System.out.println(myName);
+    }
+}
