e2f92128fd6e6718970db4d889af61191729b173
Antimka <antimkin@gmail.com>
2020-11-29 21:54:24 +0200

    adding task6 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_1/task_6/RobotDemo.java
@@ -0,0 +1,17 @@
+package students.anna_tim.lesson_3.level_1.task_6;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot myRobot1 = new Robot();
+
+        myRobot1.sayHello();
+        myRobot1.sayYourName();
+
+        Robot myRobot2 = new Robot();
+
+        myRobot2.sayHello();
+        myRobot2.sayYourName();
+    }
+}
