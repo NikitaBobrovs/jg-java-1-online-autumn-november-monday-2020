4e9e2ce5a1604d6a830abe9bcb505c6848047d5d
Antimka <antimkin@gmail.com>
2020-11-29 22:49:02 +0200

    adding task11 of lesson3

--- src/main/java/students/anna_tim/lesson_3/level_3/task_11/Robot.java
+++ src/main/java/students/anna_tim/lesson_3/level_3/task_11/Robot.java
@@ -1,10 +1,12 @@
 package students.anna_tim.lesson_3.level_3.task_11;
 
 public class Robot {
+    // в этом свойстве наш виртуальный обьект робот будет запоминать своё имя.
+    String name;
 
+    //это конструктор
     public Robot() {
-        // в этом свойстве наш виртуальный обьект робот будет запоминать своё имя.
-        String name;
+
     }
 
     public void sayHello() {
