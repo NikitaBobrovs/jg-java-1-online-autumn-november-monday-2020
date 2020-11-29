907fa4ee3aff93a64a5d60cb1783086026b1ecfb
Antimka <antimkin@gmail.com>
2020-11-29 22:03:09 +0200

    adding task7 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_2/task_8/LearnStringUsing.java
@@ -0,0 +1,10 @@
+package students.anna_tim.lesson_3.level_2.task_8;
+
+public class LearnStringUsing {
+    public static void main(String[] args) {
+
+        String myName = "Anya";
+        System.out.println("Hi "+myName+"! ;)");
+    }
+}
+
