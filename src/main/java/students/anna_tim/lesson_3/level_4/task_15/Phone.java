1e4fbb8079bf4843648acdab3811667ab13081df
Antimka <antimkin@gmail.com>
2020-11-30 00:15:58 +0200

    adding task15 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_4/task_15/Phone.java
@@ -0,0 +1,16 @@
+package students.anna_tim.lesson_3.level_4.task_15;
+
+class Phone {
+
+    String model;
+
+    Phone(String newModel) {
+        this.model = newModel;
+        }
+
+    //что это такое и зачем мы это используем?
+    String getModel() {
+        return this.model;
+        }
+
+}
