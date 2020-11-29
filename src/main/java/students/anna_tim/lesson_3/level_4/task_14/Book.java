810d66b99d0a1f12b0d7d0b42e453097f46abbb7
Antimka <antimkin@gmail.com>
2020-11-30 00:05:43 +0200

    adding task14 of lesson3

---
+++ src/main/java/students/anna_tim/lesson_3/level_4/task_14/Book.java
@@ -0,0 +1,15 @@
+package students.anna_tim.lesson_3.level_4.task_14;
+
+class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
