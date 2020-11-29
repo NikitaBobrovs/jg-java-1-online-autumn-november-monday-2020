351975f53bca49cda0fc30835ad2f09b4d6af4bc
Antimka <antimkin@gmail.com>
2020-11-30 01:51:31 +0200

    removing "public" from class definition

--- src/main/java/students/anna_tim/lesson_3/level_2/task_7/LearnString.java
+++ src/main/java/students/anna_tim/lesson_3/level_2/task_7/LearnString.java
@@ -2,7 +2,7 @@ package students.anna_tim.lesson_3.level_2.task_7;
 
 import org.w3c.dom.ls.LSOutput;
 
-public class LearnString {
+class LearnString {
     public static void main(String[] args) {
 
         String myName = "Anya";
