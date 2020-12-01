3ca97a0fa0f4becd2a3b1bd4cf91d6596955d2a7
kaverski <lena.kaverski@gmail.com>
2020-12-01 10:37:53 +0200

    Lesson 5 level 1 all tasks

---
+++ src/main/java/students/jelena_kaverska/lesson_5/level_1/task_7/ArrayLength1.java
@@ -0,0 +1,8 @@
+package students.jelena_kaverska.lesson_5.level_1.task_7;
+
+class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[numbers.length - 1]);
+    }
+}
