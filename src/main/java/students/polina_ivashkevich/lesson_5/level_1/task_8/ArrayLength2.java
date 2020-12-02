573900e779502fe8407b8a7684b91b2fc79bcdf4
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-02 15:09:00 +0200

    Lesson 5 level 1 task 8

---
+++ src/main/java/students/polina_ivashkevich/lesson_5/level_1/task_8/ArrayLength2.java
@@ -0,0 +1,10 @@
+package students.polina_ivashkevich.lesson_5.level_1.task_8;
+
+public class ArrayLength2 {
+    public static void main(String[] args) {
+        int[]numbers=new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+
+}
