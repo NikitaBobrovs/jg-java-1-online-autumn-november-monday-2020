55ef4bedc9077e0ebdc69300314212b0716e3931
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-02 14:53:03 +0200

    Lesson 5 level 1 task 5

---
+++ src/main/java/students/polina_ivashkevich/lesson_5/level_1/task_5/ArrayDefinition5.java
@@ -0,0 +1,7 @@
+package students.polina_ivashkevich.lesson_5.level_1.task_5;
+
+public class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int[] numbers =new int [3];
+    }
+}
