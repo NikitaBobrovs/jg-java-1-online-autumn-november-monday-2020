5714b3f00b70d7f27649baf6fab4988266a49ed6
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-02 13:31:06 +0200

    Lesson 3 level 7 task 32 fixed

--- src/main/java/students/polina_ivashkevich/lesson_3/level_7/task_32/BankAccount.java
+++ src/main/java/students/polina_ivashkevich/lesson_3/level_7/task_32/BankAccount.java
@@ -1,34 +1,37 @@
 package students.polina_ivashkevich.lesson_3.level_7.task_32;
 
 public class BankAccount {
-    String owner;
-        int money;
 
-        BankAccount(int moneyAmount) {
-            this.owner = owner;
-            owner = "Viktor";
-            this.money = money;
-            money = 1000;
+
+    String owner="Viktor";
+    int money;
+
+        BankAccount (String owner,int money) {
+            String moneyAmount = Integer.toString(money);
+
+
         }
+            String getOwner() {
 
-        String getOwner() {
             return this.owner;
-        }
+            }
 
-            int getMoney() {
+            int getMoney () {
+
             return this.money;
+            }
+
         }
 
-    }
 
     class BankAccountDemo {
 
         public static void main(String[] args) {
-            BankAccount bankAccount = new BankAccount(1000);
+            BankAccount bankAccount = new BankAccount("Viktor", 100000);
             String ownerFirstName = bankAccount.getOwner();
             int moneyAmount = bankAccount.getMoney();
-            System.out.println("Owner = " + bankAccount.getOwner());
-            System.out.println("Money = " + bankAccount.getMoney());
+            System.out.println("Owner = " +ownerFirstName);
+            System.out.println("Money = " + moneyAmount );
         }
 
     }
