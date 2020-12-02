8523bc4c5aaa1b44985a767363b2451a4c4e25dc
NikitaBobrovs <nekit1234@gmail.com>
2020-12-02 14:57:37 +0200

    Lesson 5 level 1 task 2 to 9

---
+++ src/main/java/students/nikita_bobrovs/lesson_5/level_1/task_2/ArrayDefinition6.java
@@ -0,0 +1,8 @@
+package students.nikita_bobrovs.lesson_5.level_1.task_2;
+
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+}
---
+++ src/main/java/students/nikita_bobrovs/lesson_5/level_1/task_2/ArrayLength3.java
@@ -0,0 +1,10 @@
+package students.nikita_bobrovs.lesson_5.level_1.task_2;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+
+}
