8fe8c7cd1edeec041b6bdef548ca988e47c009bc
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-02 15:31:44 +0200

    Lesson 5 level 1 task 10

---
+++ src/main/java/students/polina_ivashkevich/lesson_5/level_2/task_10/Class.java
@@ -0,0 +1,15 @@
+package students.polina_ivashkevich.lesson_5.level_2.task_10;
+
+import java.util.Arrays;
+
+public class Class {
+    public static void main(String[] args) {
+        int[]array=new int[3];
+        array[0]=5; array[1]=78; array[2]=32;
+        System.out.println(Arrays.toString(array));
+
+    }
+
+
+
+}
