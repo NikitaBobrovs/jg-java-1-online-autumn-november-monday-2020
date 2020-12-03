1288198f24b7f7a22aee918f934f4536c701c21e
Cath1177 <alexkatya77@gmail.com>
2020-12-03 15:05:50 +0200

    lesson5 level1 task8 (individual branch)

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_5/level_1/task_8/ArrayLength2.java
@@ -0,0 +1,10 @@
+package students.jekaterina_aleksejeva.lesson_5.level_1.task_8;
+
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+}
