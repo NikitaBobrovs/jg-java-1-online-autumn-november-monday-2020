807feb86a26a8de1b7343ed514b2de82ab356b39
NikitaBobrovs <nekit1234@gmail.com>
2020-12-03 14:35:58 +0200

    Lesson 5 level 3 task 16 to 24

---
+++ src/main/java/students/nikita_bobrovs/lesson_5/level_3/task_16/ForLoopDefinition7.java
@@ -0,0 +1,11 @@
+package students.nikita_bobrovs.lesson_5.level_3.task_16;
+
+class ForLoopDefinition7 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//task 22
