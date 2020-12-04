d5bd8a652a8559897cf5e58e2e06bbb2b71864df
ViktorChesnokov <doc18@inbox.lv>
2020-12-04 19:16:16 +0200

    lesson 5 level 1 task 2

---
+++ src/main/java/students/viktors_cesnokovs/lesson_5/level_1/task_2/ArrayDefinition2.java
@@ -0,0 +1,8 @@
+package students.viktors_cesnokovs.lesson_5.level_1.task_2;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        int []numbers = new int[10];
+    }
+}
