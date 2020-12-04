e8385439c0be73ce27de952122e3483cd45d9685
Cath1177 <alexkatya77@gmail.com>
2020-12-04 11:34:33 +0200

    lesson5 level3 task17

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_5/level_3/task_17/ForLoopDefinition2.java
@@ -0,0 +1,11 @@
+package students.jekaterina_aleksejeva.lesson_5.level_3.task_17;
+
+class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
