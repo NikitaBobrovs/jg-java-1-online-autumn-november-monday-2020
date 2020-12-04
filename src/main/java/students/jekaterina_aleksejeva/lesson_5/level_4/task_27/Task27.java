0a9165a5d737350f80d438b3bf56a8a1a49af1fa
Cath1177 <alexkatya77@gmail.com>
2020-12-04 15:26:18 +0200

    lesson5 level4 task27

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_5/level_4/task_27/Task27.java
@@ -0,0 +1,28 @@
+package students.jekaterina_aleksejeva.lesson_5.level_4.task_27;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task27 {
+    public static void main(String[] args) {
+        int[] number = new int[15];
+        Random random = new Random();
+        for (int i = 0; i < number.length; i++) {
+        number [i] = random.nextInt(15);
+        }
+
+        System.out.println("Your random array is printed: ");
+        System.out.println(Arrays.toString(number));
+        System.out.println("Maximum number in array is = " + new Task27().getMaxNum(number));
+    }
+
+    public int getMaxNum(int[] number){
+        int maxNum = number[0];
+        for (int i : number) {
+            maxNum = Math.max(maxNum, number [i]);
+        }
+        return maxNum;
+    }
+}
+
+
