d9f59d04c61ea69c19bf1996af03405b770b054d
ViktorChesnokov <doc18@inbox.lv>
2020-12-04 19:22:07 +0200

    lesson 5 level 1 task 3

--- src/main/java/students/viktors_cesnokovs/lesson_5/level_1/task_3/ArrayDefinition3.java
+++ src/main/java/students/viktors_cesnokovs/lesson_5/level_1/task_3/ArrayDefinition3.java
@@ -2,6 +2,6 @@ package students.viktors_cesnokovs.lesson_5.level_1.task_3;
 class ArrayDefinition3 {
 
     public static void main(String[] args) {
-        boolean[] numbers = new boolean [10];
+        int[] numbers = new int [10];
     }
 }
