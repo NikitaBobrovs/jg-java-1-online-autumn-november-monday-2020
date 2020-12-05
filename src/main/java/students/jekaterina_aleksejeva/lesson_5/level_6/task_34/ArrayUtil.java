412771a68c4743d6a25c0b9829a3a35cdbe94907
Cath1177 <alexkatya77@gmail.com>
2020-12-05 17:19:24 +0200

    lesson5 level6 task34

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_5/level_6/task_34/ArrayUtil.java
@@ -0,0 +1,11 @@
+package students.jekaterina_aleksejeva.lesson_5.level_6.task_34;
+
+class ArrayUtil {
+
+    public void printArrayToConsole(int[] array) {
+        System.out.println("Print out of integers of array: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
