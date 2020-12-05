de99bf1f72c401b61bdc49acef77acb6ab1a66ef
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-05 17:10:24 +0200

    Lesson 4 level 3 task 8 fixed

--- src/main/java/students/polina_ivashkevich/lesson_4/level_3/task_8/ThreeNum.java
+++ src/main/java/students/polina_ivashkevich/lesson_4/level_3/task_8/ThreeNum.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class ThreeNum {
     public static void main(String[] args) {
+
         Scanner num = new Scanner(System.in);
+        int num1;
+        int num2 ;
+        int num3;
         System.out.println("Enter three numbers :");
+        num1= num.nextInt();
+        num2=num.nextInt();
+        num3=num.nextInt() ;
 
-        int num1= num.nextInt();
-        System.out.println(num1);
-        int num2 = num.nextInt();
-        System.out.println(num2);
-        int num3 = num.nextInt();
-        System.out.println(num3);
 
-        if ((num1==num2)&&(num2==num3) ) {
+        if (num1==num2&&num2==num3&&num3==num1){
             System.out.println("All numbers are equal");
-        }if ((num1!=num2)&&(num2!=num3)) {
+        }if (num1!=num2&&num2!=num3&&num3!=num1)  {
+
             System.out.println("All numbers are different");
-        } else {
+        } if(num1!=num2&&num1==num3||num1!=num3&&num1==num2
+                ||num3!=num1&&num3==num2||num2!=num3&&num2==num1||num2!=num1&&num2==num3) {
             System.out.println("Neither all are equal or different");
         }
 
