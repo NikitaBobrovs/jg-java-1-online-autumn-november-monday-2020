b76d2764d43aa03cebf19a84460afcfe1e6497c1
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-05 20:37:49 +0200

    Lesson 4 level 4 task 15 fixed

--- src/main/java/students/polina_ivashkevich/lesson_4/level_4/task_15/Calculator.java
+++ src/main/java/students/polina_ivashkevich/lesson_4/level_4/task_15/Calculator.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Calculator {
         public int compare (int firstNumber, int secondNumber,int thirdNumber) {
             if ((firstNumber>secondNumber)&&(firstNumber>thirdNumber)) {
+                System.out.println("Biggest number is: "+ firstNumber);
                 return firstNumber;
             }if ((secondNumber>firstNumber)&&(secondNumber>thirdNumber)){
+                System.out.println("Biggest number is: "+secondNumber);
                 return secondNumber;
             } if((thirdNumber>firstNumber)&&(thirdNumber>secondNumber)){
+                System.out.println("Biggest number is: "+thirdNumber);
                 return thirdNumber;
         }   if((firstNumber==secondNumber)&&(firstNumber>thirdNumber)){
+                System.out.println("All numbers are equal");
                 return thirdNumber;
 
 
@@ -20,31 +24,7 @@ public class Calculator {
             return thirdNumber;
         }
 
-    public static void main(String[] args) {
 
-
-            java.util.Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter first number:");
-            int firstNumber = scanner.nextInt();
-            System.out.println("Enter secondNumber number");
-            int secondNumber = scanner.nextInt();
-            System.out.println("Enter third number");
-            int thirdNumber = scanner.nextInt();
-
-
-            Calculator calculator = new Calculator();
-            if ((firstNumber>secondNumber)&&(firstNumber>thirdNumber)) {
-                System.out.println("Biggest number is: "+ firstNumber);
-            }if ((secondNumber>firstNumber)&&(secondNumber>thirdNumber)) {
-                System.out.println("Biggest number is: "+secondNumber);
-            } if((thirdNumber>firstNumber)&&(thirdNumber>secondNumber)) {
-                System.out.println("Biggest number is: "+thirdNumber);
-            }if((firstNumber==secondNumber)&&(firstNumber>thirdNumber)){
-                System.out.println("Two first numbers are equal but third number is least");
-            }if((firstNumber==secondNumber)&&(secondNumber==thirdNumber)){
-                System.out.println("All numbers are equal");
-            }
-        }
     }
 
 
