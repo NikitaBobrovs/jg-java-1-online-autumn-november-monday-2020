63c3c188138bb4bbd8cae30c2df07e6c49212fb8
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-05 20:31:59 +0200

    Lesson 4 level 4 task 14 fixed

--- src/main/java/students/polina_ivashkevich/lesson_4/level_4/task_14/Calculator.java
+++ src/main/java/students/polina_ivashkevich/lesson_4/level_4/task_14/Calculator.java
@@ -3,35 +3,22 @@ package students.polina_ivashkevich.lesson_4.level_4.task_14;
 import java.util.Scanner;
 
 public class Calculator {
-    public int  maxOfTwoNumbers(int firstNumber, int secondNumber) {
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
         if (firstNumber > secondNumber) {
+            System.out.println("Biggest number is:"+ firstNumber);
+            return firstNumber;
+
+        } if (firstNumber < secondNumber) {
+            System.out.println("Biggest number is:" + secondNumber);
+            return secondNumber;
+        }else{
+            System.out.println("Two numbers are equal");
             return firstNumber;
         }
-        if (firstNumber < secondNumber) {
-            return secondNumber;
-        }
-         else  {
-             return firstNumber;
-        }
+
 
     }
 
-        public static void main (String[]args){
-            java.util.Scanner max = new Scanner(System.in);
-            System.out.println("Enter first number: ");
-            int firstNumber =max.nextInt();
-            System.out.println("Enter second number: ");
-            int secondNumber=max.nextInt();
 
-
-            Calculator calculator= new Calculator();
-            if (firstNumber>secondNumber) {
-                System.out.println("The biggest number is: "+firstNumber);
-            } if (firstNumber<secondNumber) {
-                System.out.println("The biggest number is:" +secondNumber);
-            } else {
-                System.out.println("Numbers are equals ");
-            }
-        }
-    }
+}
 
