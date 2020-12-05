ea6d0b326497ea8eaaabad2b8bdf8b8d160cee70
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-05 20:50:06 +0200

    Lesson 2 level 3 task 10 fixed

--- src/main/java/students/polina_ivashkevich/lesson_2/level_3/task_9/Integer.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_3/task_9/Integer.java
@@ -1,13 +1,11 @@
 package students.polina_ivashkevich.lesson_2.level_3.task_9;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
-import java.sql.SQLOutput;
 
 public class Integer {
     public static void main(String[] args) {
         int z =8;
-        System.out.println(z*1);
+        System.out.println(z*2);
         System.out.println(z*2);
         System.out.println(z*3);
         System.out.println(z*4);
