bd4561ffbc53e837c86bad517e77b29451ebd3cb
Antimka <antimkin@gmail.com>
2020-12-07 00:27:05 +0200

    added task3 of lesson4

---
+++ src/main/java/students/anna_tim/lesson_4/level_1/task_3/DaysOfWeek.java
@@ -0,0 +1,39 @@
+package students.anna_tim.lesson_4.level_1.task_3;
+
+import java.util.Scanner;
+
+public class DaysOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter number from 1 to 7:");
+        int number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            //необязательный блок
+            default:
+                System.out.println("Should be number from 1 to 7 :)");
+                break;
+        }
+    }
+}
