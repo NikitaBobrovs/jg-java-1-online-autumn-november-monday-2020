83bb3cf99d688a0c33251e356c131d8e813e142c
Antimka <antimkin@gmail.com>
2020-12-07 01:19:08 +0200

    added task7 of lesson4

---
+++ src/main/java/students/anna_tim/lesson_4/level_2/task_7/EqualOrDifferent.java
@@ -0,0 +1,20 @@
+package students.anna_tim.lesson_4.level_2.task_7;
+
+import java.util.Scanner;
+
+public class EqualOrDifferent {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number (should be integer):");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Please enter second number (should be integer):");
+        int num2 = scanner.nextInt();
+
+        if (num1 == num2)
+            System.out.println("Numbers "+num1+" and "+num2+" are equal.");
+        else
+            System.out.println("The numbers "+num1+" and "+num2+" are different.");
+    }
+}
