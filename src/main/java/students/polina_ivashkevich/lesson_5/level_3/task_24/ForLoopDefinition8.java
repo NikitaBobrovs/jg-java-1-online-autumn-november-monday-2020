ffb0caaa5f174ccbe7b116aa3cb940b63ec93a54
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-06 16:40:23 +0200

    Lesson 5 level 3 task 24

---
+++ src/main/java/students/polina_ivashkevich/lesson_5/level_3/task_24/ForLoopDefinition8.java
@@ -0,0 +1,9 @@
+package students.polina_ivashkevich.lesson_5.level_3.task_24;
+
+public class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for (int i=0 ;i<101 ;i++ ) {
+            System.out.println(i);
+        }
+    }
+}
