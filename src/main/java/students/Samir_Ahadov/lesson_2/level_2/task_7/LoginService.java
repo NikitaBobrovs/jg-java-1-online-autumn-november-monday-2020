28555e281ac4a7d7131c65af4b7dddf3d398f5e0
sam2319 <samirs.ahadovs@gmail.com>
2020-12-06 18:46:14 +0200

    Lesson 2 Level 2 Task 7

---
+++ src/main/java/students/Samir_Ahadov/lesson_2/level_2/task_7/LoginService.java
@@ -0,0 +1,10 @@
+package students.samir_ahadov.lesson_2.level_2.task_7;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
