8cecf71b130b28109267b319bc2581be230057b4
janase1 <janayanas21@gmail.com>
2020-12-06 14:17:51 +0200

    lesson 5 level 1 all tasks

---
+++ src/main/java/students/jana_sergejenko/lesson_5/level_1/task_1/ArrayDefinition1.java
@@ -0,0 +1,9 @@
+package students.jana_sergejenko.lesson_5.level_1.task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
---
+++ src/main/java/students/jana_sergejenko/lesson_5/level_1/task_4/ArrayDefinition4.java
@@ -0,0 +1,9 @@
+package students.jana_sergejenko.lesson_5.level_1.task_4;
+
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
