9e38b28f4726c54dd80228d8f474d0b972c4a0ef
janase1 <janayanas21@gmail.com>
2020-12-07 22:51:25 +0200

    lesson 6 level 1 all tasks

---
+++ src/main/java/students/jana_sergejenko/lesson_6/level_1/task_3/WhileDefinition3.java
@@ -0,0 +1,14 @@
+package students.jana_sergejenko.lesson_6.level_1.task_3;
+
+class WhileDefinition3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+
+        }
+
+    }
+}
---
+++ src/main/java/students/jana_sergejenko/lesson_6/level_1/task_4/WhileDefinition4.java
@@ -0,0 +1,14 @@
+package students.jana_sergejenko.lesson_6.level_1.task_4;
+
+class WhileDefinition4 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+
+}
