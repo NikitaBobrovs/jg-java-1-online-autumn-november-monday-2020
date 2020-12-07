69599dba381b82d228ac8545b61e6b7ab49a2100
Antimka <antimkin@gmail.com>
2020-12-07 02:48:07 +0200

    removing "public"

--- src/main/java/students/anna_tim/lesson_4/level_1/task_3/DaysOfWeek.java
+++ src/main/java/students/anna_tim/lesson_4/level_1/task_3/DaysOfWeek.java
@@ -2,7 +2,7 @@ package students.anna_tim.lesson_4.level_1.task_3;
 
 import java.util.Scanner;
 
-public class DaysOfWeek {
+class DaysOfWeek {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter number from 1 to 7:");
