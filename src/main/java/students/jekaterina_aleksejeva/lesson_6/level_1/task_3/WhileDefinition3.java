845317af41b4bfe20e26b0b89c8d172fc1414582
Cath1177 <alexkatya77@gmail.com>
2020-12-08 10:28:29 +0200

    lesson6 level1 task3

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_6/level_1/task_3/WhileDefinition3.java
@@ -0,0 +1,14 @@
+package students.jekaterina_aleksejeva.lesson_6.level_1.task_3;
+
+class WhileDefinition3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+
+        }
+
+    }
+}
