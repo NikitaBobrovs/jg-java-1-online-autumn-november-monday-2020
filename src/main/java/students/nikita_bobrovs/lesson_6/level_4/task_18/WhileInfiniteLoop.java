a4f7004cd04a0bfb10d8a4af0a0b905e7d08d4dd
NikitaBobrovs <nekit1234@gmail.com>
2020-12-10 15:01:24 +0200

    Lesson 6 level 4 task 18

---
+++ src/main/java/students/nikita_bobrovs/lesson_6/level_4/task_18/WhileInfiniteLoop.java
@@ -0,0 +1,10 @@
+package students.nikita_bobrovs.lesson_6.level_4.task_18;
+
+class WhileInfiniteLoop {
+    public static void main(String[] args) {
+        int i =0;
+        while (i<5) {
+            System.out.println(i--);
+        }
+    }
+}
