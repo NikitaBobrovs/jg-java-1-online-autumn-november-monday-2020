8eb0f6f56918e237f8e60acf6af981d3a23805ee
kaverski <lena.kaverski@gmail.com>
2020-12-13 17:11:28 +0200

    Lesson 6 level 7 task 32

---
+++ src/main/java/students/jelena_kaverska/lesson_6/level_7/task_32/FizzBuzz.java
@@ -0,0 +1,11 @@
+package students.jelena_kaverska.lesson_6.level_7.task_32;
+
+class FizzBuzz {
+
+    public String detect(int number) {
+        if ((number % 3 == 0) && (number % 5 == 0)) return "FizzBuzz";
+        else if (number % 3 == 0) return "Fizz";
+        else if (number % 5 == 0) return "Buzz";
+        else return "" + number;
+    }
+}
