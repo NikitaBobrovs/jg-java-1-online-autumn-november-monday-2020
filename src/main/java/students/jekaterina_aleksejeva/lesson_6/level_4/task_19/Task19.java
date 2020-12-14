a7f4b56232bedfe36ad3edc4848f9aa0fb9b5043
Cath1177 <alexkatya77@gmail.com>
2020-12-14 10:17:20 +0200

    lesson6 level4 task18 task19

--- src/main/java/students/jekaterina_aleksejeva/lesson_6/level_4/task_19/Task19.java
+++ src/main/java/students/jekaterina_aleksejeva/lesson_6/level_4/task_19/Task19.java
@@ -1,24 +1,21 @@
-package students.jekaterina_aleksejeva.lesson_6.level_4.task_18;
+package students.jekaterina_aleksejeva.lesson_6.level_4.task_19;
 
-class WhileInfiniteLoop {
+class WhileInfiniteLoopBreak {
     public static void main(String[] args) {
-
         int i = 1;
         while (true) {
             System.out.println(i++);
-
+            break;
         }
     }
-
 }
 
-class ForInfiniteLoop {
+class ForInfiniteLoopBreak {
     public static void main(String[] args) {
-
         int i = 1;
-        for ( ;; ) {
+        for (; ; ) {
             System.out.println(i++);
-
+            break;
         }
     }
 }
