7694c4c606848f2f1c83d0b693eb4d626e3c40c8
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-14 19:07:33 +0200

    Lesson 6 level 4 task 21 fixed

--- src/main/java/students/polina_ivashkevich/lesson_6/level_4/task_21/TwoDimensionalArray.java
+++ src/main/java/students/polina_ivashkevich/lesson_6/level_4/task_21/TwoDimensionalArray.java
@@ -1,20 +1,24 @@
 package students.polina_ivashkevich.lesson_6.level_4.task_21;
 
+import java.util.Random;
+
 public class TwoDimensionalArray {
 
-
-
-
-
     public static void randomNumber(){
-        int sum;
-        int[] r=new int[2];
-        r[0] =432;
-        r[1]=5436;
-        sum=r[0]+r[1];
+
+        Random random =new Random();
+        int[][] r=new int[3][3];
+        r[0][0]= random.nextInt();
+        r[1][1]= random.nextInt();
+        r[2][2]= random.nextInt();
+        int sum=r[0][0]+r[1][1]+r[2][2];
         System.out.println(sum);
+        }
+
+
     }
-}class TwoDimensionalArrayTest{
+
+class TwoDimensionalArrayTest{
     public static void main(String[] args) {
         TwoDimensionalArray array1=new TwoDimensionalArray();
         array1.randomNumber();
