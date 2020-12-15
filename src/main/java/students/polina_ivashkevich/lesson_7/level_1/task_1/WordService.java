a0391751f435b96f0f51def14dc80aeeb09e728f
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-15 21:45:56 +0200

    Lesson 7 level 1 task 1

---
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_1/WordService.java
@@ -0,0 +1,39 @@
+package students.polina_ivashkevich.lesson_7.level_1.task_1;
+
+public class WordService {
+
+    public String findMostFrequentWord(String text) {
+
+
+        return text;
+    }
+
+    public void text() {
+        String[] word = new String[10];
+        word[0] = "Cat";
+        word[1] = "Dog";
+        word[2] = "Dog";
+        word[3] = "Dog";
+        word[4] = "Parrot";
+        word[5] = "Carrot";
+        word[6] = "Parrot";
+        word[7] = "Pig";
+        word[8] = "Chicken";
+        word[9] = "Dog";
+        String a=null;
+        String b=null;
+        for (String i : word) {
+            if (i == word[1]) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_1/WordServiceTest.java
@@ -0,0 +1,12 @@
+package students.polina_ivashkevich.lesson_7.level_1.task_1;
+
+ public class WordServiceTest {
+    public static void main(String[] args) {
+        WordService WordService =new WordService();
+        WordService.text();
+
+
+        }
+
+
+}
