008de79610b879b3f663e37941ed736d8515a550
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-16 14:57:55 +0200

    Lesson 2 level 3 task 9 fixed

--- src/main/java/students/polina_ivashkevich/lesson_2/level_3/task_9/Integer.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_3/task_9/Integer.java
@@ -1,8 +1,8 @@
 package students.polina_ivashkevich.lesson_2.level_3.task_9;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
-import java.sql.SQLOutput;
+
+
 
 public class Integer {
     public static void main(String[] args) {
@@ -17,7 +17,36 @@ public class Integer {
         System.out.println(z*8);
         System.out.println(z*9);
         System.out.println(z*10);
-
+        System.out.println(z*11);
+        System.out.println(z*12);
+        System.out.println(z*13);
+        System.out.println(z*14);
+        System.out.println(z*15);
+        System.out.println(z*16);
+        System.out.println(z*17);
+        System.out.println(z*18);
+        System.out.println(z*19);
+        System.out.println(z*20);
+        System.out.println(z*21);
+        System.out.println(z*22);
+        System.out.println(z*23);
+        System.out.println(z*24);
+        System.out.println(z*25);
+        System.out.println(z*26);
+        System.out.println(z*27);
+        System.out.println(z*28);
+        System.out.println(z*29);
+        System.out.println(z*30);
+        System.out.println(z*31);
+        System.out.println(z*32);
+        System.out.println(z*33);
+        System.out.println(z*34);
+        System.out.println(z*35);
+        System.out.println(z*36);
+        System.out.println(z*37);
+        System.out.println(z*38);
+        System.out.println(z*39);
+        System.out.println(z*40);
 
     }
 
