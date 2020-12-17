2c03e5af44e936d611f4a99ab736602f1dbb8c4a
ViktorChesnokov <doc18@inbox.lv>
2020-12-17 20:00:24 +0200

    lesson 5 level 2 task 10

---
+++ src/main/java/students/viktors_cesnokovs/lesson_5/level_2/task_10/Numbers.java
@@ -0,0 +1,14 @@
+package students.viktors_cesnokovs.lesson_5.level_2.task_10;
+
+public class Numbers {
+    public static void main(String[] args) {
+        int[] number = new int[3];
+        number[0] = 5;
+        number[1] = 10;
+        number[2] = 15;
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+    }
+}
