76bb02ad0c3b684333a76321c65d91c82db3cb6e
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-18 19:21:38 +0200

    Lesson 7 level 1 task 2

---
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordService.java
@@ -0,0 +1,70 @@
+package students.polina_ivashkevich.lesson_7.level_1.task_2;
+
+public class WordService {
+
+
+
+    public String findMostFrequentWord(String text){
+          return (String) movie();
+
+    }
+
+
+
+    public Object movie() {
+        String film[]=new String[5];
+
+        film[0]="Romeo and Julietta";
+        film[1]="Pirate of the Caribbean";
+        film[2]="Romeo and Julietta";
+        film[3]="Once Upon a Time in Hollywood";
+        film[4]="Romeo and Julietta";
+        String text="Romeo and Julietta";
+        film.equals(text);
+        switch (text) {
+            case "Romeo and Julietta": {
+
+
+            }
+            case "Pirate of the Caribbean": {
+
+
+            }
+            case "Once Upon a Time in Hollywood": {
+                break;
+            }
+        }
+        System.out.println(text);
+
+        return text;
+    }
+
+    public String find() {
+        String film = new String();
+        film.equals(film);
+        switch (film) {
+            case "Romeo and Julietta": {
+                System.out.println(film);
+
+            }
+            case "Pirate of the Caribbean": {
+
+
+            }
+            case "Once Upon a Time in Hollywood": {
+                break;
+            }
+        }
+        System.out.println(film);
+
+        return film;
+    }
+    }
+
+
+
+
+
+
+
+
---
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordServiceTest.java
@@ -0,0 +1,17 @@
+package students.polina_ivashkevich.lesson_7.level_1.task_2;
+
+
+import java.sql.SQLOutput;
+
+public class WordServiceTest {
+    public static void main(String[] args) {
+        WordService  WordService=new WordService();
+        WordService.movie();
+        System.out.println( WordService.movie());
+
+
+
+    }
+
+
+}
