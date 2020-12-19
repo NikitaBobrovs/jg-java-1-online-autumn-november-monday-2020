0199a7496c033ba08063c954583a18f598465181
Konstantins Buts <kjb.dgsv@gmail.com>
2020-12-19 17:10:29 +0200

    Drop last commit history

--- src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
@@ -1,12 +1,13 @@
 package students.polina_ivashkevich.lesson_2.level_2.task_8;
 
+import java.util.Scanner;
+
 public class Name {
     public static void main(String[] args) {
-        System.out.println("Enter your name");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        String name=scanner.next();
-        System.out.println(name);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String s = scanner.nextLine();
+        System.out.println(s);
     }
 
 }
