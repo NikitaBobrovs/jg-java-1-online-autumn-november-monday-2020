bdc56442f2c20be0e4d3bcb58949b49336af7b7c
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-21 15:25:33 +0200

    Lesson 2 level 2 task 8 fixed

--- src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
+++ src/main/java/students/polina_ivashkevich/lesson_2/level_2/task_8/Name.java
@@ -1,9 +1,11 @@
 package students.polina_ivashkevich.lesson_2.level_2.task_8;
 
+import java.util.Scanner;
+
 public class Name {
     public static void main(String[] args) {
         System.out.println("Enter your name");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+       Scanner scanner = new java.util.Scanner(System.in);
         String name=scanner.next();
         System.out.println(name);
 
