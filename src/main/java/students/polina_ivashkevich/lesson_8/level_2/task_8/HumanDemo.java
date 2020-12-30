9ac2b6a4bd1ea75ffb963af9462a074ff3a77f80
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 11:56:27 +0200

    Lesson 8 level 2 task 8 second

--- src/main/java/students/polina_ivashkevich/lesson_8/level_2/task_8/HumanDemo.java
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_2/task_8/HumanDemo.java
@@ -2,6 +2,6 @@ package students.polina_ivashkevich.lesson_8.level_2.task_8;
 
 public class HumanDemo {
     public static void main(String[] args) {
-        HumanPlayer demo = new HumanPlayer("Steve");
+        new HumanPlayer("Steve");
     }
 }
