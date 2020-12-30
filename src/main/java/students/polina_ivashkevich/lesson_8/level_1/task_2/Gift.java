32613a941c556130f8afac6c33956be82ab9223b
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 10:24:19 +0200

    Lesson 8 level 1 task 2

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_2/Gift.java
@@ -0,0 +1,6 @@
+package students.polina_ivashkevich.lesson_8.level_1.task_2;
+
+public class Gift {
+        String title;
+        byte quantity;
+}
---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_2/GiftDemo.java
@@ -0,0 +1,10 @@
+package students.polina_ivashkevich.lesson_8.level_1.task_2;
+
+public class GiftDemo {
+    public static void main(String[] args) {
+        Gift gift = new Gift();
+        System.out.println("Default:");
+        System.out.println(gift.title + " title");
+        System.out.println(gift.quantity + " quantity");
+    }
+}
