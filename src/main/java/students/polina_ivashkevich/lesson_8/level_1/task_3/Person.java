66dcda64b5d011a94ee84215e81b5dfbe1f8502a
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 10:56:13 +0200

    Lesson 8 level 1 task 3

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_3/Person.java
@@ -0,0 +1,30 @@
+package students.polina_ivashkevich.lesson_8.level_1.task_3;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.http.Address;
+
+public class Person {
+    private String fullName;
+    private int age;
+    private Address address;
+
+    public void Name(String name){
+        this.fullName = name;
+
+    }
+    public void Age(int age){
+         this.age = age;
+    }
+    public void Address(Address address){
+        this.address = address;
+    }
+    public String getName(){
+        return this.fullName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public Address getAddress(){
+        return this.address;
+    }
+}
+
---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_3/PersonDemo.java
@@ -0,0 +1,10 @@
+package students.polina_ivashkevich.lesson_8.level_1.task_3;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person demo = new Person();
+        System.out.println(demo.getName() + " name");
+        System.out.println(demo.getAge() + " age");
+        System.out.println(demo.getAddress() + " address");
+    }
+}
