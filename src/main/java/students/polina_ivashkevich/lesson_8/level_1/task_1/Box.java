78c6559a6c7cc1c7aabc6e3bb94740ad810fdfb3
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 10:01:23 +0200

    Lesson 8 level 1 task 1

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_1/Box.java
@@ -0,0 +1,13 @@
+package students.polina_ivashkevich.lesson_8.level_1.task_1;
+
+public class Box {
+    int high;
+    double weight;
+
+
+    Box(){
+        high=5;
+        weight=6.1;
+
+    }
+}
