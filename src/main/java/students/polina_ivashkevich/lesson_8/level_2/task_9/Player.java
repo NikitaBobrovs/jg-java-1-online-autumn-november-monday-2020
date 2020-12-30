279859f59a8f2706b8bb7bb447ec7ea841f089ea
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 12:15:43 +0200

    Lesson 8 level 2 task 9

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_2/task_9/Player.java
@@ -0,0 +1,19 @@
+package students.polina_ivashkevich.lesson_8.level_2.task_9;
+
+public class Player {
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+}
+
+ class ComputerPlayer extends Player {
+    ComputerPlayer (String name){
+        super(name);
+    }
+
+
+}
+
