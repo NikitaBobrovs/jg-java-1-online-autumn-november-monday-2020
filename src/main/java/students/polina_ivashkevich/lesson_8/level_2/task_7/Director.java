182602dfbf2089b3886ddf708da5b9fc8b13848d
PolinaIva99 <ivashkevichpdps@gmail.com>
2020-12-30 11:35:13 +0200

    Lesson 8 level 2 task 7

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_2/task_7/Director.java
@@ -0,0 +1,14 @@
+package students.polina_ivashkevich.lesson_8.level_2.task_7;
+
+public class Director {
+    public Director(){
+        System.out.println("Hey");
+    }
+
+}
+ class Manager extends Director{
+     public static void main(String[] args) {
+
+         new Director();
+     }
+}
