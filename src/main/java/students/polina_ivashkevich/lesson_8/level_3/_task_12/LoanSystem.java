f1bb35b4318192973529dbcc0c6a87ccfb423fa2
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-02 23:40:40 +0200

    Lesson 8 level 3 task 12

--- src/main/java/students/polina_ivashkevich/lesson_8/level_3/_task_12/LoanSystem.java
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_3/_task_12/LoanSystem.java
@@ -1,8 +1,9 @@
 package students.polina_ivashkevich.lesson_8.level_3._task_12;
 
-  public abstract class LoanSystem {
-
-    abstract boolean canReceiveLoan(boolean human);
+   class LoanSystem {
+       abstract class LoanSystem1 {
+           abstract boolean canReceiveLoan(boolean human);
 
        }
+   }
 
