d8c7596e5ee06f4b2cdd063d672903b78a451a07
NikitaBobrovs <nekit1234@gmail.com>
2021-01-02 17:05:23 +0200

    Lesson 8 level 3 task 12

---
+++ src/main/java/students/nikita_bobrovs/lesson_8/level_3/task_12/Human.java
@@ -0,0 +1,4 @@
+package students.nikita_bobrovs.lesson_8.level_3.task_12;
+
+class Human {
+}
---
+++ src/main/java/students/nikita_bobrovs/lesson_8/level_3/task_12/LoanSystem.java
@@ -0,0 +1,7 @@
+package students.nikita_bobrovs.lesson_8.level_3.task_12;
+
+abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+
+}
