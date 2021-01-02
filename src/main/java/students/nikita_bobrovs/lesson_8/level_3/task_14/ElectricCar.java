80f3c46c75b6dd6b7ce1e9d41f6e12d6a9895d39
NikitaBobrovs <nekit1234@gmail.com>
2021-01-02 17:43:38 +0200

    Lesson 8 level 3 task 14

---
+++ src/main/java/students/nikita_bobrovs/lesson_8/level_3/task_14/ElectricCar.java
@@ -0,0 +1,10 @@
+package students.nikita_bobrovs.lesson_8.level_3.task_14;
+
+class ElectricCar extends Car{
+    private int batteryCharge;
+
+    @Override
+    void accelerate() {
+
+    }
+}
---
+++ src/main/java/students/nikita_bobrovs/lesson_8/level_3/task_14/PetrolCar.java
@@ -0,0 +1,10 @@
+package students.nikita_bobrovs.lesson_8.level_3.task_14;
+
+class PetrolCar extends Car {
+    private double petrolTankCapacity;
+
+    @Override
+    void accelerate() {
+
+    }
+}
