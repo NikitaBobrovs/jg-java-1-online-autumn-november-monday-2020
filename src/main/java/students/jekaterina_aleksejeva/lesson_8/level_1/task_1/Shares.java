ce69b6c73d90287b7cedb9c0087353c8fd46f398
Cath1177 <alexkatya77@gmail.com>
2021-01-02 19:13:23 +0200

    lesson8 level1 task1

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_8/level_1/task_1/Shares.java
@@ -0,0 +1,19 @@
+package students.jekaterina_aleksejeva.lesson_8.level_1.task_1;
+
+class Shares {
+    String CompanyName;
+    int numberOfShares;
+    boolean ifPaid;
+
+    Shares() {
+        System.out.println("This is default constructor");
+    }
+
+}
+
+class SharesDemo {
+    public static void main(String[] args) {
+        Shares unRegistered = new Shares();
+
+    }
+}
