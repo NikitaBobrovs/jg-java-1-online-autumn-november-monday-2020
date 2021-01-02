3122c9a778ef7c2b181c8d9c6901fd6acc6d90ed
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-02 22:28:05 +0200

    Lesson 8 level 3 task 10

---
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_3/task_10/Class.java
@@ -0,0 +1,7 @@
+package students.polina_ivashkevich.lesson_8.level_3.task_10;
+
+public class Class {
+    /*В абстрактном классе  можно определить поля и методы, но в то же время нельзя создать объект или
+    экземпляр абстрактного класса.
+     */
+}
