3cb94dc08e2ecfde0631895ab71e0de4da1f5540
ViktorChesnokov <doc18@inbox.lv>
2021-01-02 17:01:21 +0200

    lesson 6 level 4 task 19

---
+++ src/main/java/students/viktors_cesnokovs/lesson_6/level_4/task_19/ForInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package students.viktors_cesnokovs.lesson_6.level_4.task_19;
+
+public class ForInfiniteLoopBreak {
+    public static void main(String[] args) {
+        for (; ; ) {
+            System.out.println("Stop infinite");
+            break;
+        }
+    }
+}
---
+++ src/main/java/students/viktors_cesnokovs/lesson_6/level_4/task_19/WhileInfiniteLoopBreak.java
@@ -0,0 +1,11 @@
+package students.viktors_cesnokovs.lesson_6.level_4.task_19;
+
+public class WhileInfiniteLoopBreak {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i > 0) {
+            System.out.println("Stop infinite");
+            break;
+        }
+    }
+}
