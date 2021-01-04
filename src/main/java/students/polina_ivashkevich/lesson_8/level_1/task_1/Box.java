6642203810d0e7b00edab29961800e71df336453
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-04 16:38:47 +0200

    Lesson 8 level 1 task 1 fixed

--- src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_1/Box.java
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_1/Box.java
@@ -1,12 +1,11 @@
 package students.polina_ivashkevich.lesson_8.level_1.task_1;
 
 public class Box {
-    int high;
-    double weight;
 
 
-    Box(){
-        System.out.println("Default constructor");
+
+    void box(){
+
 
     }
 }
