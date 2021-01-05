344202a08b56787daebb54b49758f13f0179974a
janase1 <janayanas21@gmail.com>
2021-01-06 00:00:35 +0200

    lesson 9 level 2 all tasks

---
+++ src/main/java/students/jana_sergejenko/lesson_9/level_2/task_12/C.java
@@ -0,0 +1,7 @@
+package students.jana_sergejenko.lesson_9.level_2.task_12;
+
+class C extends B {
+    public C() {
+        System.out.println("Class C Constructor");
+    }
+}
---
+++ src/main/java/students/jana_sergejenko/lesson_9/level_2/task_9/Demo.java
@@ -0,0 +1,7 @@
+package students.jana_sergejenko.lesson_9.level_2.task_9;
+
+public class Demo {
+    public static void main(String[] args) {
+        //PrivateConstructor test=new PrivateConstructor();
+    }
+}
