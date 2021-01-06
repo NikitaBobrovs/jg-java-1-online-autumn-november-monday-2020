440bc88ec616fd19ba0ebd99a46e979862ea8868
kaverski <lena.kaverski@gmail.com>
2021-01-06 14:14:46 +0200

    Lesson 9 level 3 task 16

---
+++ src/main/java/students/jelena_kaverska/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package students.jelena_kaverska.lesson_9.level_3.task_16;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
