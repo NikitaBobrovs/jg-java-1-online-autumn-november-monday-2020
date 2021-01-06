5a4252e5b1ab9b9ec4d74543432c04cbf09917e3
kaverski <lena.kaverski@gmail.com>
2021-01-06 11:46:34 +0200

    Lesson 9 level 1 all tasks

---
+++ src/main/java/students/jelena_kaverska/lesson_9/level_1/task_4/Safe.java
@@ -0,0 +1,6 @@
+package students.jelena_kaverska.lesson_9.level_1.task_4;
+
+class Safe {
+    public String pinCode;
+    public double amount;
+}
---
+++ src/main/java/students/jelena_kaverska/lesson_9/level_1/task_7/PrivateClass.java
@@ -0,0 +1,6 @@
+package students.jelena_kaverska.lesson_9.level_1.task_7;
+
+class PrivateClass {
+    private String title;
+    private String author;
+}
