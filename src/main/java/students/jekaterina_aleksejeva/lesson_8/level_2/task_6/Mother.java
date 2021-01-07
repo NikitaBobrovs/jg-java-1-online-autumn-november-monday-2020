432c2f28bb78488d9e20ac44ee0cbbda95170888
Cath1177 <alexkatya77@gmail.com>
2021-01-07 16:45:57 +0200

    lesson8 level2 task6

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_8/level_2/task_6/Mother.java
@@ -0,0 +1,19 @@
+package students.jekaterina_aleksejeva.lesson_8.level_2.task_6;
+
+class Mother {
+
+}
+
+class Father {
+
+}
+
+class Child extends Mother {
+
+}
+
+/* Additionally: если class Father обьявить как interface Father {}, то Child сможет использовать и способности Father:
+class Child extends Mother implements Father {}
+*/
+
+
