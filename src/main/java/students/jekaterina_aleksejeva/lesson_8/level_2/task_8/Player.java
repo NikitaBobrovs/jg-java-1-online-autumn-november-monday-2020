77f4a1b15de238dae2da93359e1b221e35226ce8
Cath1177 <alexkatya77@gmail.com>
2021-01-07 19:15:16 +0200

    lesson8 level2 task8

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_8/level_2/task_8/Player.java
@@ -0,0 +1,20 @@
+package students.jekaterina_aleksejeva.lesson_8.level_2.task_8;
+
+class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+}
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+        super(name);
+
+    }
+
+}
