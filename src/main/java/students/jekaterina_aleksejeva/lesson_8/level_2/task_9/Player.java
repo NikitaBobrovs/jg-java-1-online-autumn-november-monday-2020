758eaac1d818a194cfe3c085377ef37957066154
Cath1177 <alexkatya77@gmail.com>
2021-01-07 19:21:19 +0200

    lesson8 level2 task9

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_8/level_2/task_9/Player.java
@@ -0,0 +1,19 @@
+package students.jekaterina_aleksejeva.lesson_8.level_2.task_9;
+
+class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+}
+
+class ComputerPlayer extends Player {
+
+    ComputerPlayer(String name){
+        super(name);
+    }
+
+}
