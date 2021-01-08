61aef472087faa1ea56dcd768af3d665221d7b94
Cath1177 <alexkatya77@gmail.com>
2021-01-08 14:52:18 +0200

    lesson8 level3 task14

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_8/level_3/task_14/Car.java
@@ -0,0 +1,27 @@
+package students.jekaterina_aleksejeva.lesson_8.level_3.task_14;
+
+abstract class Car {
+
+    abstract void accelerate();
+
+}
+
+class ElectricCar extends Car {
+
+    private int batteryCharge;
+
+    @Override
+    void accelerate() {
+
+    }
+}
+
+class PetrolCar extends Car {
+
+    private double petrolTankCapacity;
+
+    @Override
+    void accelerate() {
+
+    }
+}
