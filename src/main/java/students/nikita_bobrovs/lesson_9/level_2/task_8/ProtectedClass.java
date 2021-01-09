507546c5c70b5a52e2d6bce4b22d9e04c43a2643
NikitaBobrovs <nekit1234@gmail.com>
2021-01-09 11:49:15 +0200

    Lesson 9 level 2 task 8

---
+++ src/main/java/students/nikita_bobrovs/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,8 @@
+package students.nikita_bobrovs.lesson_9.level_2.task_8;
+/* Protected class would be reachable from current package
+and that's done by default/package-private.
+ */
+class ProtectedClass {
+    private String title;
+    private String author;
+}
