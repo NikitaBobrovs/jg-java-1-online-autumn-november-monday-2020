d5881035bceaaa76a71df52529c6ffe52a082b05
NikitaBobrovs <nekit1234@gmail.com>
2021-01-09 11:32:12 +0200

    Lesson 9 level 1 task 6

---
+++ src/main/java/students/nikita_bobrovs/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,11 @@
+package students.nikita_bobrovs.lesson_9.level_1.task_6;
+
+class Book {
+    private String title;
+    private String author;
+
+    Book(String title,String author){
+        this.title = title;
+        this.author = author;
+    }
+}
