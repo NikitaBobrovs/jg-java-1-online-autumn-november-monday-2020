0906737d657fa5527dd43542ac341c2e0f8dd2c2
NikitaBobrovs <nekit1234@gmail.com>
2021-01-09 10:56:34 +0200

    Lesson 9 level 1 task 4

---
+++ src/main/java/students/nikita_bobrovs/lesson_9/level_1/task_4/Safe.java
@@ -0,0 +1,6 @@
+package students.nikita_bobrovs.lesson_9.level_1.task_4;
+
+class Safe {
+    int pinCode;         //can be modified and accessed from any other class in package,
+    double moneyInSafe;  //if "public", even outside the package.
+}
