be21dfe02b4a020b3958ee7790aa952fbe5b279e
NikitaBobrovs <nekit1234@gmail.com>
2021-01-09 11:54:14 +0200

    Lesson 9 level 2 task 9

---
+++ src/main/java/students/nikita_bobrovs/lesson_9/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,6 @@
+package students.nikita_bobrovs.lesson_9.level_2.task_9;
+
+class PrivateConstructor {
+    private PrivateConstructor(){
+    }
+}
---
+++ src/main/java/students/nikita_bobrovs/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,7 @@
+package students.nikita_bobrovs.lesson_9.level_2.task_9;
+
+class PrivateConstructorDemo {
+    static void main(String[] args){
+       // PrivateConstructor demo = new PrivateConstructor();
+    }
+}
