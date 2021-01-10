77108e03332d63d3b64c3ada6df91d3c0686a5c2
Cath1177 <alexkatya77@gmail.com>
2021-01-10 17:29:31 +0200

    lesson9 level1 task1, task2, task3

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_9/level_1/task_1/Aircraft.java
@@ -0,0 +1,5 @@
+package students.jekaterina_aleksejeva.lesson_9.level_1.task_1;
+
+public class Aircraft {
+}
+//class should be public
---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_9/level_1/task_2/Rocket.java
@@ -0,0 +1,5 @@
+package students.jekaterina_aleksejeva.lesson_9.level_1.task_2;
+
+class Rocket {
+}
+// class should be default
