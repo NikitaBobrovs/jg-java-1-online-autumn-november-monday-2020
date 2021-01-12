6d7becde26ec2deb0cde486bba9d3923f68f55f5
kaverski <lena.kaverski@gmail.com>
2021-01-12 19:57:11 +0200

    Lesson 10 level 1 task 1-2

---
+++ src/main/java/students/jelena_kaverska/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -0,0 +1,12 @@
+package students.jelena_kaverska.lesson_10.level_1.task_1;
+
+class JavaStarLoginServiceImpl implements JavaStarLoginService{
+
+    @Override
+    public boolean canLogin(String login, String password) {
+        return (login != null)
+                && login.equals("javastar")
+                && (password != null)
+                && password.equals("pass");
+    }
+}
