b8f77262828550444d0f24d18ae6146e0b48dc0b
NikitaBobrovs <nekit1234@gmail.com>
2021-01-13 18:28:16 +0200

    Lesson 10 level 1 task 1

--- src/main/java/students/nikita_bobrovs/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
+++ src/main/java/students/nikita_bobrovs/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -1,7 +1,7 @@
 package students.nikita_bobrovs.lesson_10.level_1.task_1;
 
 class JavaStarLoginServiceImpl implements JavaStarLoginService {
-
+    @Override
     public boolean canLogin(String login, String password) {
         return (login != null)
                 && login.equals("javastar")
