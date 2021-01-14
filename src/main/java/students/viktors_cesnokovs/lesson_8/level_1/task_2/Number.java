da3efd6cc1176edc2a377b355baef29d4c82c709
ViktorChesnokov <doc18@inbox.lv>
2021-01-14 21:59:04 +0200

    lesson 8 level 1 task 2

---
+++ src/main/java/students/viktors_cesnokovs/lesson_8/level_1/task_2/Number.java
@@ -0,0 +1,4 @@
+package students.viktors_cesnokovs.lesson_8.level_1.task_2;
+
+public class Number {
+}
---
+++ src/main/java/students/viktors_cesnokovs/lesson_8/level_1/task_2/NumberDemo.java
@@ -0,0 +1,5 @@
+package students.viktors_cesnokovs.lesson_8.level_1.task_2;
+
+public class NumberDemo {
+    Number number = new Number();
+}
