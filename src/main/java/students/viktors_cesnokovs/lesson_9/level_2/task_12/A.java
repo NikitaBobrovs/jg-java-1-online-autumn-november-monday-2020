d68ebaef63993084b7f1c07662837f862ef894ef
ViktorChesnokov <doc18@inbox.lv>
2021-01-17 19:11:03 +0200

    lesson 9 level 2 task 12

---
+++ src/main/java/students/viktors_cesnokovs/lesson_9/level_2/task_12/A.java
@@ -0,0 +1,7 @@
+package students.viktors_cesnokovs.lesson_9.level_2.task_12;
+
+class A {
+    A() {
+        System.out.println("Class A Constructor");
+    }
+}
---
+++ src/main/java/students/viktors_cesnokovs/lesson_9/level_2/task_12/B.java
@@ -0,0 +1,7 @@
+package students.viktors_cesnokovs.lesson_9.level_2.task_12;
+
+class B extends A {
+    B() {
+        System.out.println("Class B Constructor");
+    }
+}
