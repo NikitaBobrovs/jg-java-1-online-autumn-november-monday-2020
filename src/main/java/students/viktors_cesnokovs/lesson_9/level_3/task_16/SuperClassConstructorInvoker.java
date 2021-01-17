87cd322d97e821e69ead6d452cd30059c9196f87
ViktorChesnokov <doc18@inbox.lv>
2021-01-17 19:18:23 +0200

    lesson 9 level 3 task 16

---
+++ src/main/java/students/viktors_cesnokovs/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package students.viktors_cesnokovs.lesson_9.level_3.task_16;
+
+public class SuperClassConstructorInvoker {
+    SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
