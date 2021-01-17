253949a7782b8a3a2a0906cfdc3f5498bef09586
ViktorChesnokov <doc18@inbox.lv>
2021-01-17 19:04:38 +0200

    lesson 9 level 2 task 11

---
+++ src/main/java/students/viktors_cesnokovs/lesson_9/level_2/task_11/Bread.java
@@ -0,0 +1,6 @@
+package students.viktors_cesnokovs.lesson_9.level_2.task_11;
+
+public class Bread {
+    protected Bread() {
+    }
+}
---
+++ src/main/java/students/viktors_cesnokovs/lesson_9/level_2/task_11/Sandwich.java
@@ -0,0 +1,8 @@
+package students.viktors_cesnokovs.lesson_9.level_2.task_11;
+
+class Sandwich extends Bread {
+    public static void main(String[] args) {
+        Bread sandwich = new Bread() {
+        };
+    }
+}
