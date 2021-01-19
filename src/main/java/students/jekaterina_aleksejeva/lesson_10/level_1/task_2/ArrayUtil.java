8229543ec38e558f9153b11e7cb1a797612a9df9
Cath1177 <alexkatya77@gmail.com>
2021-01-19 18:23:35 +0200

    lesson10 level1 all tasks

---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_10/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,16 @@
+package students.jekaterina_aleksejeva.lesson_10.level_1.task_2;
+
+interface ArrayUtil {
+
+    int[] createArray(int arrayLength);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+
+}
---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_10/level_1/task_3/MyList.java
@@ -0,0 +1,11 @@
+package students.jekaterina_aleksejeva.lesson_10.level_1.task_3;
+
+interface MyList {
+
+    void addElement(); // add element
+    void deleteElement(); // remove element
+    int getSize(); // update size
+    void reduceSize(); // reduce size
+    void increaseSize(); // increase size
+    void deleteAllElements(); // remove all elements
+}
