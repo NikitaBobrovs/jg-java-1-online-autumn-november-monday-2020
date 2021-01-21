6c78672f2ee9f42f81a6834b2692502d20698898
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-21 22:36:05 +0200

    Lesson 10 level 2 task 1

---
+++ src/main/java/students/polina_ivashkevich/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -0,0 +1,11 @@
+package students.polina_ivashkevich.lesson_10.level_1.task_1;
+
+public class JavaStarLoginServiceImpl {
+
+    public boolean canLogin(String login, String password) {
+        return (login != null)
+                && login.equals("javaStar")
+                && (password != null)
+                && password.equals("pass");
+    }
+}
