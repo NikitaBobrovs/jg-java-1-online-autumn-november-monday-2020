a5235cabd4f85da0b655d5ab97617269607ee909
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-21 20:17:59 +0200

    Lesson 9 level 2 task 8

---
+++ src/main/java/students/polina_ivashkevich/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,12 @@
+package students.polina_ivashkevich.lesson_9.level_2.task_8;
+
+public class ProtectedClass {
+
+    private String title;
+    private String author;
+
+    public ProtectedClass() {
+
+
+    }
+}
