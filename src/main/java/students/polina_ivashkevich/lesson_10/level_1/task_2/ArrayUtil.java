8e9df629df82e4ec5a2b050a2f01cbc3917e0705
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-01-21 22:48:49 +0200

    Lesson 10 level 2 task 2

---
+++ src/main/java/students/polina_ivashkevich/lesson_10/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,11 @@
+package students.polina_ivashkevich.lesson_10.level_1.task_2;
+
+public interface ArrayUtil {
+   void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+}
