8dcda571c3488ebed2f8a776d6824b7e009f20e0
kaverski <lena.kaverski@gmail.com>
2021-01-21 11:28:01 +0200

    Lesson 11 level 1 all tasks

---
+++ src/main/java/students/jelena_kaverska/lesson_11/task_2/Task2.java
@@ -0,0 +1,15 @@
+package students.jelena_kaverska.lesson_11.task_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Task2 {
+    public static void main(String[] args) {
+        //reference types only or primitive wrapper classes objects
+        List<Integer> ints = new ArrayList<>();
+        ints.add(1);
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book());
+    }
+}
