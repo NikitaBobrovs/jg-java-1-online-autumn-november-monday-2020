d8ef4b6cc866082d9449c09d6a044b74b0db54ed
janase1 <janayanas21@gmail.com>
2021-01-22 21:34:37 +0200

    lesson 10 level 3 task 7

---
+++ src/main/java/students/jana_sergejenko/lesson_10/level_3/task_7/Product.java
@@ -0,0 +1,13 @@
+package students.jana_sergejenko.lesson_10.level_3.task_7;
+
+public class Product {
+    private String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
