e3b9b396687a64d08e330d3161c8f49a3d8923d3
kaverski <lena.kaverski@gmail.com>
2021-01-28 12:52:05 +0200

    Lesson 7 level 1 tasks 1-2

---
+++ src/main/java/students/jelena_kaverska/lesson_7/level_1/task_1_2/WordService.java
@@ -0,0 +1,49 @@
+package students.jelena_kaverska.lesson_7.level_1.task_1_2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.*;
+
+class WordService {
+
+    public String findMostFrequentWord(String text) {
+        String[] words = text.split(",\\s*| ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        String frequentWord = " ";
+        Integer fr = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > fr) {
+                fr = entry.getValue();
+                frequentWord = entry.getKey();
+            }
+        }
+        return frequentWord;
+    }
+
+    public String findMostFrequentWord1(String text) {
+        Map<String, Long> map = Arrays.stream(text.split(",\\s*| "))
+                .collect(groupingBy(Function.identity(), counting()));
+        return map.entrySet().stream()
+                .max((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
+                .map(entry -> entry.getKey())
+                .get();
+    }
+}
+
+class My {
+    public static void main(String[] args) {
+        WordService w = new WordService();
+        System.out.println(w.findMostFrequentWord("Lorem, text,Ipsum,is,simply,dummy,text,Lorem,industry,text, Lorem, text"));
+        System.out.println(w.findMostFrequentWord1("Lorem, text,Ipsum,is,simply,dummy,text,Lorem,industry,text, Lorem, text"));
+    }
+}
---
+++ src/main/java/students/jelena_kaverska/lesson_7/level_1/task_1_2/WordServiceTest.java
@@ -0,0 +1,29 @@
+package students.jelena_kaverska.lesson_7.level_1.task_1_2;
+
+class WordServiceTest {
+    private WordService wordService = new WordService();
+
+    String printResults(boolean result) {
+        return result ? "PASSED" : "FAILED";
+    }
+
+    void testMostFrequentWord() {
+        String text = "Lorem, text,Ipsum,is,simply,dummy,text,Lorem,industry,text, Lorem";
+        String exp = "Lorem";
+        String act = wordService.findMostFrequentWord(text);
+        System.out.println("Test most frequent word: " + printResults(exp.equals(act)));
+    }
+
+    void testMostFrequentWord1() {
+        String text = "Lorem, text,Ipsum,is,simply,dummy,text,Lorem,industry,text, Lorem";
+        String exp = "Lorem";
+        String act = wordService.findMostFrequentWord1(text);
+        System.out.println("Test most frequent word: " + printResults(exp.equals(act)));
+    }
+
+    public static void main(String[] args) {
+        WordServiceTest tests = new WordServiceTest();
+        tests.testMostFrequentWord();
+        tests.testMostFrequentWord1();
+    }
+}
