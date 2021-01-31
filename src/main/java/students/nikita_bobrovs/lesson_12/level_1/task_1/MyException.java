7612bde22a099fecfb82907e1154a5e673b9c2dd
NikitaBobrovs <nekit1234@gmail.com>
2021-01-31 09:19:15 +0200

    Lesson 12 level 1 task 1 to 6

---
+++ src/main/java/students/nikita_bobrovs/lesson_12/level_1/task_1/MyException.java
@@ -0,0 +1,5 @@
+package students.nikita_bobrovs.lesson_12.level_1.task_1;
+
+class MyException extends Throwable {
+
+}
---
+++ src/main/java/students/nikita_bobrovs/lesson_12/level_1/task_3/MyException.java
@@ -0,0 +1,5 @@
+package students.nikita_bobrovs.lesson_12.level_1.task_3;
+
+class MyException extends Throwable {
+//class cannot extend multiple classes.
+}
