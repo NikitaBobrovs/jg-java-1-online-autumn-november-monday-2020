c4913014030188d19db91ed978e03efdc999a814
Eugene Trambovich <evgeny.t@20bet.com>
2021-01-31 22:14:14 +0200

    st_eugene_trambovich_lesson_8_level_1_and-level_2

---
+++ src/main/java/students/eugene_trambovich/lesson_8/level_1/task_2/Task_2.java
@@ -0,0 +1,5 @@
+package students.eugene_trambovich.lesson_8.level_1.task_2;
+
+class Task_2 {
+
+}
---
+++ src/main/java/students/eugene_trambovich/lesson_8/level_2/task_7/Info7.java
@@ -0,0 +1,11 @@
+package students.eugene_trambovich.lesson_8.level_2.task_7;
+
+ class Info7 {
+     String name;
+     int age;
+
+     public Info7(String name, int age) {
+         this.name = name;
+         this.age = age;
+     }
+ }
