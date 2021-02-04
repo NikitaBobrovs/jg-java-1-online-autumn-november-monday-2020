d865ef346e173061c9851bc10bdc3d5fe6bc3a93
Konstantins Buts <kjb.dgsv@gmail.com>
2021-02-04 20:21:58 +0200

    Bring back deleted files

--- src/main/java/students/jana_sergejenko/lesson_10/level_2/task_4_5/DayofTheWeekDetectorSwitch.java
+++ src/main/java/students/jana_sergejenko/lesson_10/level_2/task_4_5/DayofTheWeekDetectorSwitch.java
@@ -1,1 +1,35 @@
+package students.jana_sergejenko.lesson_10.level_2.task_4_5;
 
+public class DayofTheWeekDetectorSwitch implements DayOfTheWeekDetector {
+
+    // Try to use switch here!!
+    @Override
+    public String detectDayName(int number) {
+        switch (number) {
+            case 1 -> {
+                return "Monday";
+            }
+            case 2 -> {
+                return "Tuesday";
+            }
+            case 3 -> {
+                return "Wednesday";
+            }
+            case 4 -> {
+                return "Thursday";
+            }
+            case 5 -> {
+                return "Friday";
+            }
+            case 6 -> {
+                return "Saturday";
+            }
+            case 7 -> {
+                return "Sunday";
+            }
+            default -> {
+                return "Please input a valid number between 1 and 7";
+            }
+        }
+    }
+}
