42c51f933d13cfb75e61c91010e13adf8e38faa7
ViktorChesnokov <doc18@inbox.lv>
2021-02-04 16:49:28 +0200

    lesson 10 level 4 task 11

---
+++ src/main/java/students/viktors_cesnokovs/lesson_10/level_4/task_11/CelsiusConverter.java
@@ -0,0 +1,8 @@
+package students.viktors_cesnokovs.lesson_10.level_4.task_11;
+
+class CelsiusConverter implements TemperatureConverter {
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature;
+    }
+}
---
+++ src/main/java/students/viktors_cesnokovs/lesson_10/level_4/task_11/TemperatureConverter.java
@@ -0,0 +1,5 @@
+package students.viktors_cesnokovs.lesson_10.level_4.task_11;
+
+interface TemperatureConverter {
+    double convert(double celsiusTemperature);
+}
