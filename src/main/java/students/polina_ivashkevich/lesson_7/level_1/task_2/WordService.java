1e07a11f6a6786d3c59cc7aff87d9c8097d0d4ff
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-02-06 19:33:15 +0200

    Lesson 7 level 1 task 2 fixed

--- src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordService.java
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordService.java
@@ -3,10 +3,8 @@ package students.polina_ivashkevich.lesson_7.level_1.task_2;
 public class WordService {
 
 
-
     public String findMostFrequentWord(String text){
           return (String) movie();
-
     }
 
 
@@ -59,7 +57,7 @@ public class WordService {
 
         return film;
     }
-    }
+}
 
 
 
--- src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordServiceTest.java
+++ src/main/java/students/polina_ivashkevich/lesson_7/level_1/task_2/WordServiceTest.java
@@ -1,8 +1,5 @@
 package students.polina_ivashkevich.lesson_7.level_1.task_2;
 
-
-
-
 public class WordServiceTest {
     public static void main(String[] args) {
         WordService  WordService=new WordService();
