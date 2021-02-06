ebd5998045b2f2838952065345bb584548a811e0
PolinaIva99 <ivashkevichpdps@gmail.com>
2021-02-06 20:01:32 +0200

    Lesson 10 level 1 task 1 fixed

--- src/main/java/students/polina_ivashkevich/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
+++ src/main/java/students/polina_ivashkevich/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -1,6 +1,6 @@
 package students.polina_ivashkevich.lesson_10.level_1.task_1;
 
-public class JavaStarLoginServiceImpl {
+public class JavaStarLoginServiceImpl implements JavaStarLoginService {
 
     public boolean canLogin(String login, String password) {
         return (login != null)
