07b6c1a795b2878eeed8cf14cc64c2a2460f1919
kaverski <lena.kaverski@gmail.com>
2021-02-09 22:27:55 +0200

    Lesson 14 level 3-6

---
+++ src/main/java/students/jelena_kaverska/lesson_14/level_3_6/Transaction.java
@@ -0,0 +1,48 @@
+package students.jelena_kaverska.lesson_14.level_3_6;
+
+import java.util.Objects;
+
+class Transaction {
+    private Trader trader;
+    private int year;
+    private int value;
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return this.trader;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String toString() {
+        return "{" + this.trader + ", " +
+                "year: "+this.year+", " +
+                "value:" + this.value +"}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return year == that.year &&
+                value == that.value &&
+                Objects.equals(trader, that.trader);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trader, year, value);
+    }
+}
