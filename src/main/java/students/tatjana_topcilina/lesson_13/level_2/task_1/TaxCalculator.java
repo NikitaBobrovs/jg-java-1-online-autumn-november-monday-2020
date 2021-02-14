c98877aff979ab34d63158667945c06efe1b8fcc
topchilina <topchilina@gmail.com>
2021-02-15 01:23:13 +0200

    lesson_3.level_1.task1_2

---
+++ src/main/java/students/tatjana_topcilina/lesson_13/level_2/task_1/TaxCalculator.java
@@ -0,0 +1,8 @@
+package students.tatjana_topcilina.lesson_13.level_2.task_1;
+
+import java.math.BigDecimal;
+
+public interface TaxCalculator {
+    BigDecimal calculateTax(BigDecimal income);
+}
+
---
+++ src/main/java/students/tatjana_topcilina/lesson_3/level_1/task1_2/Robot.java
@@ -0,0 +1,14 @@
+package students.tatjana_topcilina.lesson_3.level_1.task1_2;
+
+public class Robot {
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+    public Robot() {
+
+    }
+
+    public void sayYourName() {
+    }
+}
