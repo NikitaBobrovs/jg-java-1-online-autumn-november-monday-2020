01fe79f7e3af3e5386ebc438fef7e63a35094c9d
topchilina <topchilina@gmail.com>
2021-02-14 19:01:03 +0200

    lesson_8.level_2.task_5_6

---
+++ src/main/java/students/tatjana_topcilina/lesson_8/level_2/task_5_6/Mother.java
@@ -0,0 +1,19 @@
+package students.tatjana_topcilina.lesson_8.level_2.task_5_6;
+
+public class Mother {
+
+}
+
+class Father {
+
+}
+
+class Child extends Mother {
+// OR class Child extends Father {}
+}
+
+
+
+
+//Можественное наследование в Java запрещено
+//Можественное наследование - это гарантия того, что класс может быть наследником не только одного класса, но многих
