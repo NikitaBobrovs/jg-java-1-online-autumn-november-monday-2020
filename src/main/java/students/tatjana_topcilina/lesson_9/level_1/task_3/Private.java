7ee8a58ba45000715254b875e2439c135c363681
topchilina <topchilina@gmail.com>
2021-02-14 19:56:02 +0200

    lesson_9.level_1.task_3

---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_1/task_3/Private.java
@@ -0,0 +1,6 @@
+package students.tatjana_topcilina.lesson_9.level_1.task_3;
+
+public class Private {
+}
+// Можно сделать класс Private,
+// и он будет скрываться от других классов и доступен для кода только внутри данного класса
