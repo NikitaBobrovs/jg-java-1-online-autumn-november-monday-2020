7cd955ee4db2f518746208ab35f5bdf8629b3e8d
topchilina <topchilina@gmail.com>
2021-02-14 20:38:27 +0200

    lesson_9.level_1.task_9

---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,9 @@
+package students.tatjana_topcilina.lesson_9.level_2.task_9;
+
+public class PrivateConstructorDemo {
+    public static void main(String[] args) {
+       // PrivateConstructor test=new PrivateConstructor();
+
+    }
+
+}
