1dd58c532b3284f1324890e0aeab8364736a722a
topchilina <topchilina@gmail.com>
2021-02-14 17:01:58 +0200

    lesson_5.level_1.task_4

---
+++ src/main/java/students/tatjana_topcilina/lesson_5/level_1/task_4/ArrayDefinition4.java
@@ -0,0 +1,7 @@
+package students.tatjana_topcilina.lesson_5.level_1.task_4;
+
+public class ArrayDefinition4 {
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+    }
+}
