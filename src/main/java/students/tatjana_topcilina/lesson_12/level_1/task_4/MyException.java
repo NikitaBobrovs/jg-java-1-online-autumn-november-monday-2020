01750e4dbe15f61cb457843466026611c5b25473
topchilina <topchilina@gmail.com>
2021-02-15 00:29:23 +0200

    lesson_12.level_1.task_4

--- src/main/java/students/tatjana_topcilina/lesson_12/level_1/task_4/MyException.java
+++ src/main/java/students/tatjana_topcilina/lesson_12/level_1/task_4/MyException.java
@@ -1,8 +1,9 @@
 package students.tatjana_topcilina.lesson_12.level_1.task_4;
 
  public class MyException {
-    public MyException(String message) {
-        super(message);
+     public MyException(String message) {
+         super (message);
 
-    }
-}
+     }
+ }
+
