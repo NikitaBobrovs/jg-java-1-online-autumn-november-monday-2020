d1686cd6dafe5684459a984754efcd0fc58aec98
topchilina <topchilina@gmail.com>
2021-02-14 20:08:33 +0200

    lesson_9.level_1.task_4

---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_1/task_4/Safe.java
@@ -0,0 +1,9 @@
+package students.tatjana_topcilina.lesson_9.level_1.task_4;
+
+public class Safe {
+    int pinCode= 111;
+    double money = 10.00;
+
+}
+
+
---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_1/task_4/SafeDemo.java
@@ -0,0 +1,9 @@
+package students.tatjana_topcilina.lesson_9.level_1.task_4;
+
+public class SafeDemo {
+    public static void main(String[] args) {
+        Safe safe = new Safe();
+        System.out.println("PinCode : " + safe.pinCode);
+        System.out.println("Money amount : " + safe.money + " EUR");
+    }
+}
