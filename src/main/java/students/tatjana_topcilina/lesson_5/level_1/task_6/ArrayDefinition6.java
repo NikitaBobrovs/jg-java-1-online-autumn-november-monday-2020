0ccd6e3cd5e78928476a66603fdbda00fdf0fd98
topchilina <topchilina@gmail.com>
2021-02-14 17:03:58 +0200

    lesson_5.level_1.task_6

---
+++ src/main/java/students/tatjana_topcilina/lesson_5/level_1/task_6/ArrayDefinition6.java
@@ -0,0 +1,7 @@
+package students.tatjana_topcilina.lesson_5.level_1.task_6;
+
+public class ArrayDefinition6 {
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+    }
+}
