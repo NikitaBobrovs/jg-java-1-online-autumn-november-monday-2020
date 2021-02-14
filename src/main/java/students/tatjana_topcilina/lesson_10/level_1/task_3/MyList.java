4b1448efa9097adaa3355556dad1b2f026b2293d
topchilina <topchilina@gmail.com>
2021-02-14 21:48:16 +0200

    lesson_10.level_1.task_3

---
+++ src/main/java/students/tatjana_topcilina/lesson_10/level_1/task_3/MyList.java
@@ -0,0 +1,11 @@
+package students.tatjana_topcilina.lesson_10.level_1.task_3;
+
+interface MyList {
+    void add();  //add the element at the specified position in a list
+    void remove(); //remove element
+    void clear (); //remove all elements
+
+    Object[] toArray(); // return array containing all elements in the list
+
+
+}
