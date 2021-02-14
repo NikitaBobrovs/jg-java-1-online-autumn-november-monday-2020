21a606d115b457d324808a04a3b07218ad44d9be
topchilina <topchilina@gmail.com>
2021-02-14 17:13:47 +0200

    lesson_5.level_2.task_10

---
+++ src/main/java/students/tatjana_topcilina/lesson_5/level_2/task_10/WholeNumber.java
@@ -0,0 +1,17 @@
+package students.tatjana_topcilina.lesson_5.level_2.task_10;
+
+public class WholeNumber {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+        numbers[0]=1;
+        numbers[1]=2;
+        numbers[2]=3;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+    }
+
+}
