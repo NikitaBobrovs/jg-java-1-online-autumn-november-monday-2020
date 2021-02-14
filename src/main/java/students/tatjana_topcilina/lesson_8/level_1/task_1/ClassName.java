362125f69f4fb2a9b095205c49234b63ac97671d
topchilina <topchilina@gmail.com>
2021-02-14 18:22:31 +0200

    lesson_8.level_1.task_1

---
+++ src/main/java/students/tatjana_topcilina/lesson_8/level_1/task_1/ClassName.java
@@ -0,0 +1,18 @@
+package students.tatjana_topcilina.lesson_8.level_1.task_1;
+
+public class ClassName {
+
+        ClassName(){
+
+        }
+    }
+
+
+    class ClassDemo {
+        public static void main(String[] args) {
+            ClassName classname = new ClassName();
+
+        }
+
+    }
+
