7074a586f5ccbb08795a2325b2c20759a917fae6
topchilina <topchilina@gmail.com>
2021-02-14 16:14:56 +0200

    Lesson_3.level_2.task_7

---
+++ src/main/java/students/tatjana_topcilina/lesson_3/level2/task7/MyName.java
@@ -0,0 +1,8 @@
+package students.tatjana_topcilina.lesson_3.level2.task7;
+
+public class MyName {
+    public static void main(String[] args) {
+        String myName = "Tanja";
+        System.out.println(myName);
+    }
+}
