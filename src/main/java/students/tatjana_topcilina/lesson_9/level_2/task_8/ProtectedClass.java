40a4ccc4948a5c3c9c6e40133db82005872402d2
topchilina <topchilina@gmail.com>
2021-02-14 20:29:53 +0200

    lesson_9.level_1.task_8

---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,10 @@
+package students.tatjana_topcilina.lesson_9.level_2.task_8;
+
+public class ProtectedClass {
+    private String title;
+    private String author;
+
+
+}
+
+// Нельзя тк они будут скрыты. В практике protected используют для содержания
