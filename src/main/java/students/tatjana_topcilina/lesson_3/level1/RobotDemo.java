ac1973091c8b0283cbfcc0da75a6fc56e212395f
topchilina <topchilina@gmail.com>
2021-02-14 14:55:19 +0200

    Lesson_3.level_1.task_1.task_2

---
+++ src/main/java/students/tatjana_topcilina/lesson_3/level1/RobotDemo.java
@@ -0,0 +1,24 @@
+package students.tatjana_topcilina.lesson_3.level1;
+
+import students.tatjana_topcilina.lesson_3.level1.task1_2.Robot;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+        myRobot.sayHello();
+        myRobot.sayYourName();
+
+        Robot myRobot2 = new Robot ();
+        myRobot2.sayHello();
+        myRobot2.sayYourName();
+    }
+
+        public void sayHello() {
+            System.out.println("Hello!"); }
+
+        public void sayYourName() {
+            System.out.println("My name is ROBO"); }
+
+
+        }
