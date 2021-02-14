a0f828272d798bcca5abb0b9c6af03f2eef0fa6f
topchilina <topchilina@gmail.com>
2021-02-14 17:05:11 +0200

    lesson_5.level_1.task_7

---
+++ src/main/java/students/tatjana_topcilina/lesson_5/level_1/task_7/ArrayLength1.java
@@ -0,0 +1,8 @@
+package students.tatjana_topcilina.lesson_5.level_1.task_7;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length -1);
+    }
+}
