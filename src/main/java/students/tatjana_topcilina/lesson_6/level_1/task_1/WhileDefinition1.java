1650fd01b0e9a9fd508bf78f9496754e0b53a521
topchilina <topchilina@gmail.com>
2021-02-14 17:24:49 +0200

    lesson_6.level_1.task_1

---
+++ src/main/java/students/tatjana_topcilina/lesson_6/level_1/task_1/WhileDefinition1.java
@@ -0,0 +1,12 @@
+package students.tatjana_topcilina.lesson_6.level_1.task_1;
+
+public class WhileDefinition1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
