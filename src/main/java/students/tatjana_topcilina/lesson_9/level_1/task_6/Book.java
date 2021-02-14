e821273884ae0cb02eb2ad075c9fcf6d5900d2eb
topchilina <topchilina@gmail.com>
2021-02-14 20:15:10 +0200

    lesson_9.level_1.task_6

---
+++ src/main/java/students/tatjana_topcilina/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,11 @@
+package students.tatjana_topcilina.lesson_9.level_1.task_6;
+
+public class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
