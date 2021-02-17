7d4954458702ec5669650c93537c0a05bcaba07b
Cath1177 <alexkatya77@gmail.com>
2021-02-17 20:44:34 +0200

    lesson13 level1 all tasks

--- src/main/java/students/jekaterina_aleksejeva/lesson_12/level_3/task_22/task22reply.java
+++ src/main/java/students/jekaterina_aleksejeva/lesson_12/level_3/task_22/task22reply.java
@@ -1,6 +1,6 @@
 package students.jekaterina_aleksejeva.lesson_12.level_3.task_22;
 
 public class task22reply {
-    //класс Error в Java является главным в иерархии классов ошибок
+    //класс Throwable в Java является главным в иерархии классов ошибок
 
 }
---
+++ src/main/java/students/jekaterina_aleksejeva/lesson_13/level_1/task_1/TaxCalculator.java
@@ -0,0 +1,8 @@
+package students.jekaterina_aleksejeva.lesson_13.level_1.task_1;
+
+import java.math.BigDecimal;
+
+interface TaxCalculator {
+    BigDecimal calculateTax(BigDecimal income);
+
+}
