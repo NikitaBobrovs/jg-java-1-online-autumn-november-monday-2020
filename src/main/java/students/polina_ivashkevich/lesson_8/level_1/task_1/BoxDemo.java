92062c2b12760f9529789c7807e3fc9571f5b5ee
Konstantins Buts <kjb.dgsv@gmail.com>
2021-02-20 16:10:42 +0200

    Fix errors

--- src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_1/BoxDemo.java
+++ src/main/java/students/polina_ivashkevich/lesson_8/level_1/task_1/BoxDemo.java
@@ -3,9 +3,10 @@ package students.polina_ivashkevich.lesson_8.level_1.task_1;
 public class BoxDemo {
     public static void main(String[] args) {
         Box box = new Box();
-        box.box();
 
     }
+}
 
+class Box {
 
 }
